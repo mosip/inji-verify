bcbc8d332b7f873b3d56f9bc77068cd25abb2d7e
Shiva <shivakumar@beehyv.com>
2024-04-01 10:19:47 +0530

    Added DID resolution Signed-off-by: Shiva <shivakumar@beehyv.com>

--- src/utils/verification-utils.js
+++ src/utils/verification-utils.js
@@ -1,10 +1,17 @@
-import {verifyCredential/*, downloadRevocationList*/} from 'verification-sdk';
-import {SAMPLE_DID, SAMPLE_VERIFIABLE_CREDENTIAL} from "./samples";
+import {verifyCredential/*, downloadRevocationList*/} from "verification-sdk";
+import {resolveDid} from "./did-utils";
 
-export const verify = async (vc) => {
-    // download revocation list
-    // resolve did
-    let status = await verifyCredential(SAMPLE_DID,  SAMPLE_VERIFIABLE_CREDENTIAL, []);
-    console.log("VC Status: ", status);
-    return status;
+let revocationList = [];
+
+const verify = async (credential) => {
+    let resolutionResult = await resolveDid(credential.proof.verificationMethod);
+    if (resolutionResult.didResolutionMetadata.error) {
+        throw new Error(resolutionResult.didResolutionMetadata.error)
+    }
+    let issuerDID = resolutionResult.didDocument;/*
+    let revocationUrl = "http://localhost:3000/credentials/revocation-list";*/
+    revocationList = /*await downloadRevocationList(issuerDID.id, revocationUrl)*/[];
+    return  await verifyCredential(issuerDID, credential, revocationList);
 }
+
+export {verify};
