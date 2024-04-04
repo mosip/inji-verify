447534cba69bc3cd4b0a31bb9df40fb9464db637
Shiva <shivakumar@beehyv.com>
2024-04-04 18:43:25 +0530

    Added Snackbar message after loading the qr from file Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationSection/ImportFromFile.tsx
+++ inji-verify/src/components/Home/VerificationSection/ImportFromFile.tsx
@@ -2,8 +2,22 @@ import {scanFilesForQr} from "../../../utils/qr-utils";
 import {QrScanResult} from "../../../types/data-types";
 import {SetScanResultFunction} from "../../../types/function-types";
 import StyledButton from "./commons/StyledButton";
+import {Alert, Snackbar} from "@mui/material";
+import {useState} from "react";
+
+const AlertMessages = {
+    success: "QR code uploaded successfully!",
+    sessionExpired: "The scan session has expired due to inactivity. Please initiate a new scan.",
+    qrNotDetected: "No MultiFormat Readers were able to detect the QR code."
+};
 
 export const ImportFromFile = ({setScanResult}: {setScanResult: SetScanResultFunction}) => {
+    const [snackbarMessage, setSnackbarMessage] = useState(AlertMessages.success);
+
+    function handleSnackbarClose () {
+        setSnackbarMessage("");
+    }
+
     return (
         <div style={{margin: "12px auto", display: "grid", placeContent: "center"}}>
             <StyledButton style={{width: "350px"}}>
@@ -26,9 +40,34 @@ export const ImportFromFile = ({setScanResult}: {setScanResult: SetScanResultFun
                     if (!file) return;
                     scanFilesForQr(file)
                         .then(scanResult => {
+                            if (scanResult.data === null) {
+                                setSnackbarMessage(AlertMessages.qrNotDetected);
+                            }
+                            setSnackbarMessage(AlertMessages.success);
                             setScanResult(scanResult);
                         });
                 }}
             />
+            <Snackbar
+                open={!!snackbarMessage}
+                autoHideDuration={6000}
+                onClose={() => setSnackbarMessage("")}
+                message={snackbarMessage}
+                anchorOrigin={{vertical: "top", horizontal: "right"}}
+            >
+                <Alert
+                    onClose={handleSnackbarClose}
+                    severity={snackbarMessage === AlertMessages.success ? "success" : "error"}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                    style={{
+                        borderRadius: '10px',
+                        padding: '16px 18px'
+                    }}
+
+                >
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
         </div>);
 }
