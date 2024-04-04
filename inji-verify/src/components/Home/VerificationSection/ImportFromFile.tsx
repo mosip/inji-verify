ac6144c6a30f7763b58f0f90b07761a2016f30e3
Shiva <shivakumar@beehyv.com>
2024-04-04 09:53:20 +0530

    Add styling to upload button Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationSection/ImportFromFile.tsx
+++ inji-verify/src/components/Home/VerificationSection/ImportFromFile.tsx
@@ -1,18 +1,24 @@
 import {scanFilesForQr} from "../../../utils/qr-utils";
 import {QrScanResult} from "../../../types/data-types";
 import {SetScanResultFunction} from "../../../types/function-types";
+import StyledButton from "./commons/StyledButton";
 
 export const ImportFromFile = ({setScanResult}: {setScanResult: SetScanResultFunction}) => {
     return (
         <div style={{margin: "12px auto", display: "grid", placeContent: "center"}}>
-            <label htmlFor={"upload-qr"} style={{font: 'normal normal normal 16px/21px Inter'}}>Upload your certificate</label><br/>
+            <StyledButton>
+                <label htmlFor={"upload-qr"}>
+                    Upload your certificate</label>
+            </StyledButton>
+            <br/>
             <input
                 type="file"
                 id="upload-qr"
                 name="upload-qr"
                 accept=".png, .jpeg, .jpg, .pdf"
                 style={{
-                    margin: "8px auto"
+                    margin: "8px auto",
+                    display: "none"
                 }}
                 onChange={e => {
                     const file = e?.target?.files && e?.target?.files[0];
@@ -22,6 +28,6 @@ export const ImportFromFile = ({setScanResult}: {setScanResult: SetScanResultFun
                             setScanResult(scanResult);
                         });
                 }}
-            />`
+            />
         </div>);
 }
