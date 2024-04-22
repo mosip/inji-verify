2201e35e8fbe9e641c00f0f66211a1ae8aa7cc5d
Shiva <shivakumar@beehyv.com>
2024-04-22 18:37:24 +0530

    [INJIVER-150] Preload the images required for offline functionality and store in cache Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/commons/PreloadImages.tsx
+++ inji-verify/src/components/commons/PreloadImages.tsx
@@ -1,28 +1,8 @@
 // PreloadImages.js
-import React, { useEffect } from 'react';
+import React from 'react';
 
+//Preload the images required for offline use case
 const PreloadImages = ({ imageUrls }: {imageUrls: string[]}) => {
-    useEffect(() => {
-        const preloadImages = async () => {
-            try {
-                const cache = await caches.open('image-cache');
-                await Promise.all(
-                    imageUrls.map(async imageUrl => {
-                        const cachedResponse = await cache.match(imageUrl);
-                        if (!cachedResponse) {
-                            await fetch(imageUrl).then(response => {
-                                cache.put(imageUrl, response.clone());
-                            });
-                        }
-                    })
-                );
-            } catch (error) {
-                console.error('Error preloading images:', error);
-            }
-        };
-
-        preloadImages();
-    }, [imageUrls]);
     return (
         <>
             {imageUrls.map(url => (<img src={url} style={{display: "none"}} width={0}/>))}
