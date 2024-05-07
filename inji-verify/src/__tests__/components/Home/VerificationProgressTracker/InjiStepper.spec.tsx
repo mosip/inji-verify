83c90b157fd37296ccc4b3ad2b5f898a2d32661c
Shiva <shivakumar@beehyv.com>
2024-05-07 16:25:31 +0530

    [INJIVER-155] Added tests for VerificationProgressTracker Signed-off-by: Shiva <shivakumar@beehyv.com>

---
+++ inji-verify/src/__tests__/components/Home/VerificationProgressTracker/InjiStepper.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import InjiStepper from "../../../../components/Home/VerificationProgressTracker/InjiStepper";
+import {VerificationSteps, VerificationStepsContent} from "../../../../utils/config";
+import {Provider} from "react-redux";
+import configureMockStore from "redux-mock-store";
+
+const mockStore = configureMockStore();
+const store = mockStore({activeScreen: VerificationSteps.ScanQrCodePrompt});
+
+describe("Stepper Content Header", () => {
+    test("Test rendering", () => {
+        render(
+            <Provider store={store}>
+                <InjiStepper/>
+            </Provider>
+        )
+
+        VerificationStepsContent.forEach(step => {
+            expect(screen.getByText(step.label)).toBeInTheDocument();
+            if (typeof step.description === "string") {
+                expect(screen.getByText(step.description)).toBeInTheDocument();
+            }
+        })
+    })
+})
--- inji-verify/src/components/Home/VerificationProgressTracker/InjiStepper.tsx
+++ inji-verify/src/components/Home/VerificationProgressTracker/InjiStepper.tsx
@@ -1,37 +1,19 @@
 import React from 'react';
 import {useAppSelector} from "../../../redux/hooks";
 import {VerificationStep} from "../../../types/data-types";
-
-const steps: VerificationStep[] = [
-    {
-        label: 'Select ‘Scan QR Code’ or ‘Upload QR Code’ ',
-        description: ['Tap \'Scan QR Code\' and scan to capture the QR code shown on your digital credentials/card.', 'Tap ‘Upload QR Code’ to upload the preferred file.'],
-    },
-    {
-        label: 'Activate your device’s camera',
-        description: 'Activate your device camera for scanning: A notification will be prompt to activate your device camera (Valid for ‘Scan QR Code’ feature only)',
-    },
-    {
-        label: 'Verify document',
-        description: 'Allow Inji Verify to verify & validate the digital document / card'
-    },
-    {
-        label: 'View result',
-        description: 'Check the validation result'
-    }
-];
+import {VerificationStepsContent} from "../../../utils/config";
 
 function InjiStepper() {
     const activeScreen = useAppSelector(state => state.activeScreen);
-    const isLastStep = (index: number) => steps.length -1 === index;
+    const isLastStep = (index: number) => VerificationStepsContent.length -1 === index;
     const isStepCompleted = (index: number) => activeScreen >= index;
 
     return (
         <div className="flex flex-col items-start justify-start ml-0 mt-9">
             <div className="flex flex-col items-start space-y-2">
                 {
-                    steps.map((step, index) => (
-                        <>
+                    VerificationStepsContent.map((step, index) => (
+                        <div  key={step.label}>
                             <div className="flex items-center">
                                 <div
                                     className={`text-center rounded-full w-6 h-6 flex items-center justify-center font-normal text-normal text-[12px] leading-5  ${isStepCompleted(index) ? "bg-primary text-white border-1 border-none" : "bg-white text-primary border-[1px] border-primary"}`}
@@ -53,7 +35,7 @@ function InjiStepper() {
                                     )
                                 }
                             </div>
-                        </>
+                        </div>
                     ))
                 }
             </div>
