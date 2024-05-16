034d1fdcd14a86590630defcc3891590017d336e
Shiva <shivakumar@beehyv.com>
2024-05-16 20:31:56 +0530

    [INJIVER-117] Integrated with wellknown endpoint to get the VC Display Properties Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationSection/Result/VcDisplayCard.tsx
+++ inji-verify/src/components/Home/VerificationSection/Result/VcDisplayCard.tsx
@@ -1,35 +1,99 @@
-import React from 'react';
-import {Box, Grid, Typography} from '@mui/material';
-import {convertToTitleCase, getDisplayValue} from "../../../../utils/misc";
+import React, {useEffect, useState} from 'react';
+import {Box, Divider, Grid, Typography} from '@mui/material';
+import {convertToTitleCase, fetchWellknownProperties, getDisplayValue} from "../../../../utils/misc";
 import StyledButton from "../commons/StyledButton";
 import {SAMPLE_VERIFIABLE_CREDENTIAL} from "../../../../utils/samples";
 import {SetActiveStepFunction} from "../../../../types/function-types";
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import {VerificationSteps} from "../../../../utils/config";
 import {VcDisplay, VcProperty, VcPropertyKey, VcPropertyValue, VcVerificationFailedContainer} from "./styles";
+import {constructWellKnownUrlFromDid} from "../../../../utils/did-utils";
+import Loader from "../../../commons/Loader";
+
+
+const DisplayVc = ({loadingWellKnown, credentialDisplayProperties, vc}: {credentialDisplayProperties: any, loadingWellKnown: boolean, vc: any}) => {
+    return loadingWellKnown ?
+        (<Box style={{width: "100%", margin: 4}}>
+            <Loader style={{margin: "12px auto"}}/>
+            <Box style={{padding: "12px 0px", width: "100%", textAlign: "center"}}>
+                <p>Displaying the credential...</p>
+            </Box>
+        </Box>)
+        : (<>
+            {
+                Object.keys(credentialDisplayProperties ?? vc.credentialSubject)
+                    .filter(key => key !== "id" && key !== "type")
+                    .map((key, index) => (
+                        <VcProperty item xs={12} lg={6} key={key}>
+                            <VcPropertyKey>
+                                {credentialDisplayProperties ? credentialDisplayProperties[key].display[0].name : convertToTitleCase(key)}
+                            </VcPropertyKey>
+                            <VcPropertyValue>
+                                {getDisplayValue(vc.credentialSubject[key])}
+                            </VcPropertyValue>
+                        </VcProperty>
+                    ))
+            }
+        </>)
+}
 
 function VcDisplayCard({vc, setActiveStep}: {vc: any, setActiveStep: SetActiveStepFunction}) {
+    const [wellKnownProperties, setWellKnownProperties] = useState<any>();
+    const [loadingWellKnown, setLoadingWellKnown] = useState(true);
+
+    useEffect(() => {
+        fetchWellknownProperties(constructWellKnownUrlFromDid(vc?.issuer))
+            .then(response => {
+                setWellKnownProperties(response);
+            })
+            .catch(error => {
+                setWellKnownProperties(null);
+            })
+            .finally(() => {setLoadingWellKnown(false)})
+    }, []);
+
+    const credentialDetails = wellKnownProperties?.credentials_supported ? wellKnownProperties?.credentials_supported[0] : null;
+    const credentialDisplayProperties: any = credentialDetails?.credential_definition?.credentialSubject;
+
     return (
         <Box style={{paddingBottom: 60}}>
             <VcDisplay container>
                 {
-                    vc ? Object.keys(vc.credentialSubject)
-                        .filter(key => key?.toLowerCase() !== "id" && key?.toLowerCase() !== "type")
-                        .map(key => (
-                            <VcProperty item xs={12} lg={6} key={key}>
-                                <VcPropertyKey>
-                                    {convertToTitleCase(key)}
-                                </VcPropertyKey>
-                                <VcPropertyValue>
-                                    {getDisplayValue(vc.credentialSubject[key])}
-                                </VcPropertyValue>
-                            </VcProperty>
-                        ))
+                    credentialDisplayProperties && vc && (
+                        <>
+                            <Box style={{padding: "10px 4px", width: "100%", textAlign: "center"}}
+                                className={`justify-start py-2.5 px-1 xs:col-end-13 w-[100%] text-center`}
+                            >
+                                <img style={{margin: "4px auto"}}
+                                     width={100}
+                                     src={credentialDetails?.display[0].logo.url}
+                                     alt={credentialDetails?.display[0].logo.alt_text}
+                                />
+                                <Typography style={{fontWeight: 600, fontSize: 15, margin: "4px auto"}} className="font-medium text-[14px] mx-auto my-1">
+                                    {credentialDetails?.display[0].name}
+                                </Typography>
+                            </Box>
+                            <Divider style={{width: "100%"}}/>
+                        </>
+                    )
+                }
+                {
+                    vc ? (<DisplayVc credentialDisplayProperties={credentialDisplayProperties} loadingWellKnown={loadingWellKnown} vc={vc}/>)
                         : (
                             <VcVerificationFailedContainer>
                                 <DescriptionOutlinedIcon fontSize={"inherit"} color={"inherit"}/>
                             </VcVerificationFailedContainer>
                         )
+                }
+                {
+                    credentialDisplayProperties && vc && (
+                        <>
+                            <Divider style={{width: "100%"}}/>
+                            <Typography style={{fontSize: 12, textAlign: "right", width: "100%", padding: "10px 5px"}} className="text-[12px] text-right w-[100%] pr-3">
+                                Issued by <b>{wellKnownProperties.credential_issuer}</b>
+                            </Typography>
+                        </>
+                    )
                 }
             </VcDisplay>
             <Box style={{
--- inji-verify/src/utils/did-utils.ts
+++ inji-verify/src/utils/did-utils.ts
@@ -19,3 +19,12 @@ export const resolveDid = async (did: string)  => {
 
     return await didResolver.resolve(did);
 }
+
+export const constructWellKnownUrlFromDid = (issuerId: string) => {
+    // Define a regular expression to extract the URL
+    const regex = /did:web:([^:]+:[^#]+)/;
+    const match = issuerId?.match(regex);
+    // Extract the URL from the matched groups
+    return match ? `https://${match[1].replaceAll(":", "/")}/.wellknown/openid-credential-issuer` : null;
+}
+
