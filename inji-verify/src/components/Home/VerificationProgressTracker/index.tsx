ede431b0b398d3afd8a0b619552785d83caaab44
Shiva <shivakumar@beehyv.com>
2024-05-25 02:51:16 +0530

    [INJIVER-77] Added Mobile Stepper Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationProgressTracker/index.tsx
+++ inji-verify/src/components/Home/VerificationProgressTracker/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import InjiStepper from "./InjiStepper";
+import DesktopStepper from "./DesktopStepper";
+import MobileStepper from "./MobileStepper";
 
 function VerificationProgressTracker() {
     return (
         <div className="bg-[#FAFBFD] bg-no-repeat pt-0 pb-[100px] pr-[60px] pl-[76px] mt-0">
-            <InjiStepper/>
+            <DesktopStepper/>
+            <MobileStepper/>
         </div>
     );
 }
