e203b3587687595a432d290895f0418d7a01c0f0
Shiva <shivakumar@beehyv.com>
2024-05-31 12:03:31 +0530

    [INJIVER-77] Updated content Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/Header/index.tsx
+++ inji-verify/src/components/Home/Header/index.tsx
@@ -4,13 +4,12 @@ import VerificationMethodTabs from "./VerificationMethodTabs";
 function Header(props: any) {
     return (
         <div>
-            <div className="w-[100%] bg-[#F2FCFF] py-9 max-w-[100vw] text-center">
-                <p className="mx-auto my-3 font-bold text-[26px] max-w-[80vw]">
+            <div className="w-[100%] bg-[#F2FCFF] py-[26px] md:py-9 max-w-[100vw] text-center">
+                <p className="mx-auto my-1.5 font-bold text-[19px] md:text-[26px] max-w-[80vw]">
                     Verify credentials <span className="text-primary">with ease!</span>
                 </p>
-                <p className="mx-auto my-1.5 text-[16px] font-normal max-w-[80vw] md:max-w-[624px]">
-                    Effortlessly verify credentials with Inji Verify! Just scan the QR code or upload it for instant
-                    validation, following the steps below.
+                <p className="mx-auto my-1.5 text-[14px] md:text-[16px] font-normal px-[22px] md:max-w-[624px]">
+                    Effortlessly verify credentials with Inji Verify! Just scan the QR code or upload it for instant validation, following the steps below.
                 </p>
             </div>
             <VerificationMethodTabs/>
