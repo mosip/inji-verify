8e5422e7b68220a488af47c0be7d86c8908a20ab
Shiva <shivakumar@beehyv.com>
2024-05-31 08:12:29 +0530

    [INJIVER-77] Updated mobile stepper Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationSection/Result/ResultSummary.tsx
+++ inji-verify/src/components/Home/VerificationSection/Result/ResultSummary.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {ReactComponent as VerificationSuccessIcon} from "../../../../assets/verification-success-icon.svg";
 import {ReactComponent as VerificationFailedIcon} from "../../../../assets/verification-failed-icon.svg";
+import {ReactComponent as VerificationSuccessMobileIcon} from "../../../../assets/verification-success-icon-mobile.svg";
+import {ReactComponent as VerificationFailedMobileIcon} from "../../../../assets/verification-failed-icon-mobile.svg";
+
 
 const ResultSummary = ({success}: {
     success: boolean
 }) => {
     return (
         <div className="grid">
-            <div className="xs:col-end-13">
+            <div className="xs:col-end-13 md:hidden text-center bg-white text-black pb-8">
+                <p className="font-bold text-lg">
+                    View Results
+                </p>
+                <p>
+                    View the verification result.
+                </p>
+            </div>
+            <div className={`xs:col-end-13 h-[170px] md:h-[340px] ${success ? "bg-[#4B9D1F]" : "bg-[#CB4242]"}`}>
                 <div className="grid justify-items-center items-center justify-center content-center pt-[30px]">
                     <div className={`col-end-13 hidden md:grid rounded-[50%] bg-white h-[68px] w-[68px] content-center justify-center text-[24px] mx-auto my-[7px] ${success ? "bg-[#4B9D1F]": "bg-[#CB4242]"}`}>
                         {success ? <VerificationSuccessIcon/> : <VerificationFailedIcon/>}
                     </div>
-                    <div className="block md:hidden">
-                        {success ? <VerificationSuccessIcon/> : <VerificationFailedIcon/>}
+                    <div className="col-end-13 block md:hidden mb-2.5">
+                        {success ? <VerificationSuccessMobileIcon/> : <VerificationFailedMobileIcon/>}
                     </div>
                     <div className="hidden md:block col-end-13">
                         <p className="font-bold text-[20px]  mx-auto my-[7px]">
--- inji-verify/src/components/Home/VerificationSection/Result/index.tsx
+++ inji-verify/src/components/Home/VerificationSection/Result/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import ResultSummary from "./ResultSummary";
 import VcDisplayCard from "./VcDisplayCard";
 import {useVerificationFlowSelector} from "../../../../redux/features/verification/verification.selector";
@@ -6,16 +6,27 @@ import {useVerificationFlowSelector} from "../../../../redux/features/verificati
 const Result = () => {
     const {vc, vcStatus} = useVerificationFlowSelector(state => state.verificationResult ?? {vc: null, vcStatus: null})
     let success = vcStatus?.status === "OK";
+    const [vcCardPosition, setVcCardPosition] = useState<{top?: string, left?: string} | undefined>();
+
+    const positionReference: any = useRef();
+    useEffect(() => {
+        console.log({vcCardPosition})
+        if (positionReference?.current && !vcCardPosition) {
+            let resultSectionPosition = positionReference?.current?.getBoundingClientRect();
+            console.log({resultSectionPosition})
+            setVcCardPosition({top: `${resultSectionPosition.bottom - 100}px`})
+        }
+    }, [positionReference?.current]);
     // validate vc and show success/failure component
     return (
         <div id="result-section">
-            <div className={`h-[170px] md:h-[340px] text-white ${success ? "bg-[#4B9D1F]" : "bg-[#CB4242]"}`}>
+            <div className={`text-white`}>
                 <ResultSummary success={success}/>
             </div>
             <div
                 className={`absolute m-auto`}
                 style={{
-                    top: window.innerWidth >= 768 ? `532px` : `585px`,
+                    top: window.innerWidth >= 768 ? `532px` : "684px",
                     right: window.innerWidth >= 768 ? `calc((50vw - 400px) / 2)` : `calc((100vw - 340px) / 2)`
                 }}>
                 <VcDisplayCard vc={vcStatus?.status === "OK" ? vc : null}/>
