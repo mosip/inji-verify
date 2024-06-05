b9bf87b5ecf0ad6241b9ca2ac428a95caa9187aa
Shiva <shivakumar@beehyv.com>
2024-06-05 20:18:52 +0530

    [INJIVER-284] Added responsiveness for the offline page Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/pages/Offline.tsx
+++ inji-verify/src/pages/Offline.tsx
@@ -3,7 +3,7 @@ import SomethingWentWrong from "../components/SomethingWentWrong";
 
 function Offline(props: any) {
     return (
-        <div className="py-[46px] px-[80px] bg-[#FAFBFD] bg-no-repeat h-[100%]">
+        <div className="py-[46px] px-[15px] lg:px-[80px] bg-[#FAFBFD] bg-no-repeat h-[100%]">
             <img id="inji-verify-logo" src='/assets/images/inji_verify.svg' alt="inji-verify-logo"/>
             <SomethingWentWrong/>
         </div>
