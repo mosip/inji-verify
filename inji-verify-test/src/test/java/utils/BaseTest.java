85ec9402630b3cd4ae6dee71604cfb244d498906
Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>
2024-06-07 13:39:26 +0530

    INJIVER-203 - Framework development Inji-Verify UI automation (#67)
    * INJIVER-250 Inji Verify UI automation Basic framework
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Validation for homepage
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Enhanced the framework for multiple environments
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Enhanced the framework for multiple environments
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * INJIVER-203
    Signed-off-by: Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>
    ---------
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    Signed-off-by: Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>

---
+++ inji-verify-test/src/test/java/utils/BaseTest.java
@@ -0,0 +1,46 @@
+package utils;
+
+import org.json.JSONObject;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class BaseTest {
+
+	public static Playwright playwright;
+	public static Browser browser;
+
+	protected static final String ENVIRONMENT = System.getProperty("env") == null ? "qa-inji"
+			: System.getProperty("env");
+
+	BaseTestUtil baseTestUtil = new BaseTestUtil();
+
+	DriverManager driver;
+
+	public BaseTest(DriverManager driver) {
+		this.driver = driver;
+	}
+
+	@Before
+	public void setup() {
+		playwright = Playwright.create();
+		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Page page = browser.newPage();
+		driver.setPage(page);
+		JSONObject config = baseTestUtil.readConfig(BaseTest.class, ENVIRONMENT);
+		String url = config.getString("url");
+		driver.getPage().navigate(url);
+	}
+
+	@After
+	public void tearDown() {
+		driver.getPage().close();
+		browser.close();
+		playwright.close();
+	}
+}
