87dbda42f044064ce31ac00ee247efc755ca83fa
Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>
2024-06-07 13:39:26 +0530

    INJIVER-203 - Framework development Inji-Verify UI automation (#67)
    * INJIVER-250 Inji Verify UI automation Basic framework
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Validation for homepage
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Enhanced the framework for multiple environments
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * Enhanced the framework for multiple environments
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    * INJIVER-203
    Signed-off-by: Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>
    ---------
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    Signed-off-by: Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>

---
+++ inji-verify-test/src/test/java/constants/UiConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class UiConstants {
+
+	public static final String PAGE_TITLE = "Inji Verify";
+	public static final String PAGE_HEADER = "Verify credentials with ease!";
+	public static final String PAGE_SUB_HEADER = "Effortlessly verify credentials with Inji Verify! Just scan the QR code or upload it for instant validation, following the steps below.";
+}
