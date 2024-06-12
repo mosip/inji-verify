66e3307cbc5c4a2286db9510d7a9b58a9e588c8f
Kumar M <pkumarm@deloitte.com>
2024-06-12 18:06:58 +0530

    use env config for icon,favicom

--- inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
+++ inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {NavbarContainer} from "./styles";
 
 function Navbar(props: any) {
+    const logoImageName = process.env.REACT_APP_LOGO_IMAGE;
     // Logo goes here
     return (
         <NavbarContainer>
-            <img src='/assets/images/inji_verify.svg'/>
+            <img src={`/assets/images/${logoImageName}`}/>
         </NavbarContainer>
     );
 }
