f175aa5e2f4a7f7fa06687717853f4add5d7dbcc
Kumar M <pkumarm@deloitte.com>
2024-06-17 16:52:41 +0530

    Revert "use env config for icon,favicom"
    This reverts commit 9834c9813272aca785236d3b3b79bfb3ed76cc45.
    
    Signed-off-by: Kumar M <pkumarm@deloitte.com>

--- inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
+++ inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import {NavbarContainer} from "./styles";
 
 function Navbar(props: any) {
-    const logoImageName = process.env.REACT_APP_LOGO_IMAGE;
     // Logo goes here
     return (
         <NavbarContainer>
-            <img src={`/assets/images/${logoImageName}`}/>
+            <img src='/assets/images/inji_verify.svg'/>
         </NavbarContainer>
     );
 }
