46fa3f86318075f14d8f7cc2e6dbd4aa80174f9f
Shiva <shivakumar@beehyv.com>
2024-07-04 15:30:18 +0530

    INJIVER-418 Check for the internet connection after selecting a file - Upload flow Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-verify/src/components/Home/VerificationSection/UploadQrCode.tsx
+++ inji-verify/src/components/Home/VerificationSection/UploadQrCode.tsx
@@ -7,38 +7,44 @@ import {raiseAlert} from "../../../redux/features/alerts/alerts.slice";
 import {checkInternetStatus, getFileExtension} from "../../../utils/misc";
 import {updateInternetConnectionStatus} from "../../../redux/features/application-state/application-state.slice";
 import {AlertInfo} from "../../../types/data-types";
+import {Dispatch} from "redux";
 
-const doFileChecks = (file: File): AlertInfo | null => {
+const doFileChecks = (dispatch: Dispatch, file: File | null): boolean => {
+    if (!file) return false;
+    let alert: AlertInfo | null = null;
     // file format check
     const fileExtension = getFileExtension(file.name);
     if (!SupportedFileTypes.includes(fileExtension)) {
-        return AlertMessages.unsupportedFileType;
+        alert = AlertMessages.unsupportedFileType;
     }
 
     // file size check
     if (file.size < UploadFileSizeLimits.min || file.size > UploadFileSizeLimits.max) {
-        return AlertMessages.unsupportedFileSize;
+        alert = AlertMessages.unsupportedFileSize;
     }
 
-    return null;
+    if (alert) {
+        dispatch(goHomeScreen({}));
+        dispatch(raiseAlert({...alert, open: true}))
+        return false;
+    }
+    return true;
+}
+
+const doInternetCheck = async (dispatch: Dispatch) => {
+    dispatch(updateInternetConnectionStatus({internetConnectionStatus: "LOADING"}));
+    let isOnline = await checkInternetStatus();
+    dispatch(updateInternetConnectionStatus({internetConnectionStatus: isOnline ? "ONLINE" : "OFFLINE"}));
+    return isOnline;
 }
 
 const acceptedFileTypes = SupportedFileTypes.map(ext => `.${ext}`).join(', ')
 
 function UploadButton({ displayMessage }: {displayMessage: string}) {
-    const dispatch = useAppDispatch();
     return (
         <label
             className="hover:bg-primary bg-[#FFFFFF] hover:text-[#FFFFFF] text-primary bg-no-repeat rounded-[9999px] border-2 border-primary font-bold w-[350px] cursor-pointer text-center px-0 py-[12px] text-[16px] fill-[#ff7f00] hover:fill-white"
             htmlFor={"upload-qr"}
-            onClick={async (event) => {
-                dispatch(updateInternetConnectionStatus({internetConnectionStatus: "LOADING"}));
-                event.stopPropagation();
-                event.preventDefault();
-                let isOnline = await checkInternetStatus();
-                dispatch(updateInternetConnectionStatus({internetConnectionStatus: isOnline ? "ONLINE" : "OFFLINE"}));
-                if (isOnline) document.getElementById("upload-qr")?.click();
-            }}
         >
             <span className="flex m-auto content-center justify-center w-[100%]">
                 <span className="inline-grid mr-1.5">
@@ -64,17 +70,18 @@ export const UploadQrCode = ({displayMessage, className}: { displayMessage: stri
                 name="upload-qr"
                 accept={acceptedFileTypes}
                 className="mx-auto my-2 hidden h-0"
-                onChange={e => {
+                onChange={async (e) => {
+                    const isOnline = await doInternetCheck(dispatch);
+                    if (!isOnline) return;
+
                     const file = e?.target?.files && e?.target?.files[0];
-                    if (!file) return;
-                    const alert = doFileChecks(file);
-                    if (alert) {
-                        dispatch(goHomeScreen({}));
-                        dispatch(raiseAlert({...alert, open: true}))
+                    const fileChecksPassed = doFileChecks(dispatch, file);
+                    if (!fileChecksPassed) {
                         if (e?.target)
                             e.target.value = ""; // clear the target to be able to read same file again
                         return;
                     }
+
                     dispatch(qrReadInit({method: "UPLOAD"}));
                     scanFilesForQr(file)
                         .then(scanResult => {
