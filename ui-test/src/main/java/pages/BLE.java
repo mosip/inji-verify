f0a9ce1a00553aab031a6b3c5df835becfee5794
maheswaras <maheswara.s@cyberpwn.com>
2024-07-11 17:07:18 +0530

    MOSIP-34141 (#115)
    * MOSIP-34141
    Signed-off-by: maheswaras <maheswara.s@cyberpwn.com>
    
    * MOSIP-34141
    Signed-off-by: maheswaras <maheswara.s@cyberpwn.com>
    
    * MOSIP-34141
    Signed-off-by: maheswaras <maheswara.s@cyberpwn.com>
    
    * MOSIP-34141
    Signed-off-by: maheswaras <maheswara.s@cyberpwn.com>
    
    ---------
    
    Signed-off-by: maheswaras <maheswara.s@cyberpwn.com>

---
+++ ui-test/src/main/java/pages/BLE.java
@@ -0,0 +1,32 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BLE extends BasePage {
+
+	private WebDriver driver;
+
+	public BLE(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//button[@id='ble-tab']")
+	WebElement bleTab;
+
+	@FindBy(xpath = "//p[@id='alert-message']")
+	WebElement bleAlertMsg;
+
+	public void ClickonBleTab() {
+		clickOnElement(driver, bleTab);
+	}
+
+	public String getInformationText() {
+		return getText(driver, bleAlertMsg);
+	}
+}
