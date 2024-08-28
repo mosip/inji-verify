dd8d6e12526d11249b507eea8b0fececfe0d1c75
Srikanth Kola <97477121+srikanth716@users.noreply.github.com>
2024-08-28 11:44:19 +0530

    [INJIVER-127] Update QR code scanning and upload libraries (#137)
    * handeling durian changes from inji web
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-127] chore: Update QR code scanning and upload libraries
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-127] removed unknown dependency
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-444]: update readme file and added fictious assets
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-444]: Organized fictitious assets by placing them in the  directory
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-444]: updated readme with available tags,release branches and active branches
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    ---------
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- inji-verify/src/components/Home/VerificationSection/Verification.tsx
+++ inji-verify/src/components/Home/VerificationSection/Verification.tsx
@@ -1,43 +1,49 @@
-import React from 'react';
+import React from "react";
 import scanQr from "../../../assets/scanner-ouline.svg";
 import Loader from "../../commons/Loader";
 import QrScanner from "./QrScanner";
 import StyledButton from "./commons/StyledButton";
-import {useAppDispatch} from "../../../redux/hooks";
-import {goHomeScreen} from "../../../redux/features/verification/verification.slice";
-import {VerificationSteps} from "../../../utils/config";
-import {useVerificationFlowSelector} from "../../../redux/features/verification/verification.selector";
+import { useAppDispatch } from "../../../redux/hooks";
+import { goHomeScreen } from "../../../redux/features/verification/verification.slice";
+import { VerificationSteps } from "../../../utils/config";
+import { useVerificationFlowSelector } from "../../../redux/features/verification/verification.selector";
+import { terminateScanning } from "../../../utils/qr-utils";
 
 const Verification = () => {
-    const dispatch = useAppDispatch();
-    const {activeScreen, method} = useVerificationFlowSelector(state => ({activeScreen: state.activeScreen, method: state.method}));
-    console.log({activeScreen});
-    return (
-        <div
-            className="grid grid-cols-12 mx-auto pt-1 pb-[100px] px-[16px] lg:py-[42px] lg:px-[104px] text-center content-center justify-center">
-            <div
-                className="col-start-1 col-end-13 grid w-[100%] lg:w-[350px] aspect-square max-w-[280px] lg:max-w-none bg-cover content-center justify-center m-auto"
-                style={{
-                    backgroundImage: `url(${scanQr})`
-                }}>
-                {
-                    activeScreen === VerificationSteps[method].Verifying
-                        ? (<Loader/>)
-                        : (<QrScanner/>)
-                }
-            </div>
-            <div className="col-span-12">
-                <StyledButton
-                    id="verification-back-button"
-                    className="w-[100%] lg:w-[350px] max-w-[280px] lg:max-w-none mt-[18px]"
-                    onClick={() => {
-                        dispatch(goHomeScreen({}))
-                    }}>
-                    Back
-                </StyledButton>
-            </div>
-        </div>
-    );
-}
+  const dispatch = useAppDispatch();
+  const { activeScreen, method } = useVerificationFlowSelector((state) => ({
+    activeScreen: state.activeScreen,
+    method: state.method,
+  }));
+  console.log({ activeScreen });
+  return (
+    <div className="grid grid-cols-12 mx-auto pt-1 pb-[100px] px-[16px] lg:py-[42px] lg:px-[104px] text-center content-center justify-center">
+      <div
+        className="col-start-1 col-end-13 grid w-[100%] lg:w-[350px] aspect-square max-w-[280px] lg:max-w-none bg-cover content-center justify-center m-auto"
+        style={{
+          backgroundImage: `url(${scanQr})`,
+        }}
+      >
+        {activeScreen === VerificationSteps[method].Verifying ? (
+          <Loader />
+        ) : (
+          <QrScanner />
+        )}
+      </div>
+      <div className="col-span-12">
+        <StyledButton
+          id="verification-back-button"
+          className="w-[100%] lg:w-[350px] max-w-[280px] lg:max-w-none mt-[18px]"
+          onClick={() => {
+            terminateScanning();
+            dispatch(goHomeScreen({}));
+          }}
+        >
+          Back
+        </StyledButton>
+      </div>
+    </div>
+  );
+};
 
 export default Verification;
--- inji-verify/src/utils/qr-utils.js
+++ inji-verify/src/utils/qr-utils.js
@@ -1,35 +1,77 @@
-import { scanFile } from "@openhealthnz-credentials/pdf-image-qr-scanner";
-import {decode, generateQRData} from '@mosip/pixelpass';
-import {HEADER_DELIMITER, SUPPORTED_QR_HEADERS} from "./config";
+import { decode, generateQRData } from "@mosip/pixelpass";
+import { HEADER_DELIMITER, SUPPORTED_QR_HEADERS } from "./config";
+import { Html5Qrcode } from "html5-qrcode";
+import { pdfToQrData } from "./pdfToQrData";
 
 export const scanFilesForQr = async (selectedFile) => {
-    let scanResult = { data: null, error: null };
-    try {
-        scanResult.data = await scanFile(selectedFile);
-    } catch (e) {
-        // Example Error Handling
-        if (e?.name === "InvalidPDFException") {
-            scanResult.error = "Invalid PDF";
-        } else if (e instanceof Event) {
-            scanResult.error = "Invalid Image";
-        } else {
-            scanResult.error = "Unknown error:" + e;
-        }
+  let scanResult = { data: null, error: null };
+  const html5QrCode = new Html5Qrcode("upload-qr");
+  try {
+    if (selectedFile.type === "application/pdf") {
+      const qrResult = await pdfToQrData(selectedFile);
+      scanResult.data = qrResult;
+    } else {
+      const qrData = await html5QrCode.scanFile(selectedFile);
+      scanResult.data = qrData;
     }
-    return scanResult;
-}
+  } catch (e) {
+    // Example Error Handling
+    if (e?.name === "InvalidPDFException") {
+      scanResult.error = "Invalid PDF";
+    } else if (e instanceof Event) {
+      scanResult.error = "Invalid Image";
+    } else {
+      scanResult.error = "Unknown error:" + e;
+    }
+  }
+  return scanResult;
+};
 
 export const decodeQrData = (qrData) => {
-    if (!(!!qrData)) return;
-    let encodedData = qrData
-    if (!!HEADER_DELIMITER) {
-        const splitQrData = qrData.split(HEADER_DELIMITER);
-        const header = splitQrData[0];
-        if (SUPPORTED_QR_HEADERS.indexOf(header) === -1) return; // throw some error and handle it
-        if (splitQrData.length !== 2) return; // throw some error and handle it
-        encodedData = splitQrData[1];
-    }
-    return decode(encodedData);
-}
+  if (!!!qrData) return;
+  let encodedData = qrData;
+  if (!!HEADER_DELIMITER) {
+    const splitQrData = qrData.split(HEADER_DELIMITER);
+    const header = splitQrData[0];
+    if (SUPPORTED_QR_HEADERS.indexOf(header) === -1) return; // throw some error and handle it
+    if (splitQrData.length !== 2) return; // throw some error and handle it
+    encodedData = splitQrData[1];
+  }
+  return decode(encodedData);
+};
 
 export const encodeData = (data) => generateQRData(data);
+
+let html5QrCode;
+
+export const initiateQrScanning = (timer, onSuccess) => {
+  const config = {
+    fps: 10,
+    disableFlip: false,
+    aspectRatio: 1.0,
+  };
+  if (!html5QrCode?.getState()) {
+    html5QrCode = new Html5Qrcode("reader");
+    const qrCodeSuccessCallback = (decodedText) => {
+      onSuccess(decodedText);
+      html5QrCode.stop();
+      html5QrCode = null;
+    };
+
+    html5QrCode
+      .start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
+      .catch((e) => {
+        console.error("Error occurred:", e.message);
+        clearTimeout(timer);
+        html5QrCode.stop();
+        html5QrCode = null;
+      });
+  }
+};
+
+export const terminateScanning = () => {
+  if (html5QrCode) {
+    html5QrCode.stop();
+    html5QrCode = null;
+  }
+};
