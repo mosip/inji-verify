494c68aaf29010dfeabd4c2f2268d6187861a6a7
srikanth716 <srikanthsri7447@gmail.com>
2024-09-03 21:56:27 +0530

    [INJIVER-601] fix:Back button from scan page is bringing the upload ovp last event
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- inji-verify/src/pages/OvpRedirect.tsx
+++ inji-verify/src/pages/OvpRedirect.tsx
@@ -13,9 +13,9 @@ function OvpRedirect(props: any) {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        let vpToken, presentationSubmission, params, error, errorDescription;
+        let vpToken, presentationSubmission, error, errorDescription;
         try {
-            params = new URLSearchParams(location.hash.substring(1));
+            const params = new URLSearchParams(location.hash.substring(1));
             const queryParams = new URLSearchParams(location.search.substring(1));
 
             vpToken = !!params.get("vp_token")
@@ -32,11 +32,9 @@ function OvpRedirect(props: any) {
             console.error("Error occurred while reading params in redirect url, Error: ", error);
         }
         finally {
+            navigate(Pages.Home, { replace: true });
             if (!!vpToken && !!presentationSubmission) {
                 dispatch(verificationInit({ovp: {vpToken, presentationSubmission}}));
-                params?.delete("vp_token");
-                params?.delete("presentation_submission");
-                navigate(Pages.Home, { replace: true });
             } else if (!!error) {
                 dispatch(raiseAlert(
                     {
