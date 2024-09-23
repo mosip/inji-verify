8b432eb2e670e0c6d7a8a34fc17b9ba6d4066ede
srikanth716 <srikanthsri7447@gmail.com>
2024-09-23 20:42:03 +0530

    [INJIVER-613] fix: pdf qrcode upload issue with zing wasm library
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- inji-verify/src/utils/qr-utils.js
+++ inji-verify/src/utils/qr-utils.js
@@ -1,33 +1,63 @@
 import { decode, generateQRData } from "@mosip/pixelpass";
 import { HEADER_DELIMITER, SUPPORTED_QR_HEADERS } from "./config";
-import { pdfToQrData } from "./pdfToQrData";
+import * as pdfjsLib from "pdfjs-dist/webpack";
 
 const zxing = await window.ZXing();
 
-const readBarcodes = async (file) => {
-  let format = "QRCode";
-
-  let arrayBuffer = await file.arrayBuffer();
-  let u8Buffer = new Uint8Array(arrayBuffer);
-
-  let zxingBuffer = zxing._malloc(u8Buffer.length);
+const readQRcodeFromImageFile = async (file, format, isPDF) => {
+  const arrayBuffer = await file.arrayBuffer();
+  const u8Buffer = new Uint8Array(arrayBuffer);
+  const zxingBuffer = zxing._malloc(u8Buffer.length);
   zxing.HEAPU8.set(u8Buffer, zxingBuffer);
-  let results = zxing.readBarcodesFromImage(
+  const results = zxing.readBarcodesFromImage(
     zxingBuffer,
     u8Buffer.length,
     true,
     format,
     0xff
   );
+
   zxing._free(zxingBuffer);
 
   if (results.size() === 0) {
-    throw new Error("No " + ({ format } || "barcode") + " found");
+    if (!isPDF) {
+      throw new Error(`No ${format} found`);
+    }
   } else {
     for (let i = 0; i < results.size(); i += 1) {
       const { text } = results.get(i);
       return escapeTags(text);
     }
+  }
+};
+
+const readQRcodeFromPdf = async (file, format) => {
+  const pdfData = await file.arrayBuffer();
+  const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
+  let result;
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const viewport = page.getViewport({ scale: 2.0 });
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    await page.render(renderContext).promise;
+    const dataURL = canvas.toDataURL();
+    const blob = await (await fetch(dataURL)).blob();
+    const qrCode = await readQRcodeFromImageFile(blob, format, true);
+    if (qrCode) {
+      result = qrCode;
+    }
+  }
+  if (result) {
+    return result;
+  } else {
+    throw new Error(`No ${format} found`);
   }
 };
 
@@ -40,16 +70,19 @@ function escapeTags(htmlStr) {
     .replace(/'/g, "&#39;");
 }
 
-
 export const scanFilesForQr = async (selectedFile) => {
   let scanResult = { data: null, error: null };
+  const format = "QRCode";
+
   try {
-    if (selectedFile.type === "application/pdf") {
-      const qrResult = await pdfToQrData(selectedFile);
+    const fileType = selectedFile.type;
+
+    if (fileType === "application/pdf") {
+      const qrResult = await readQRcodeFromPdf(selectedFile, format);
       scanResult.data = qrResult;
     } else {
-      const qrData = await readBarcodes(selectedFile);
-      scanResult.data = qrData;
+      const qrResult = await readQRcodeFromImageFile(selectedFile, format);
+      scanResult.data = qrResult;
     }
   } catch (e) {
     // Example Error Handling
