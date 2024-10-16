8d4512b5c80e3fed3d027bfcb80e0ae7473bae1b
srikanth716 <srikanthsri7447@gmail.com>
2024-10-16 15:22:55 +0530

    [INJIVER-655] moved outline condition to common config
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- ui/public/env.config.js
+++ ui/public/env.config.js
@@ -1,5 +1,8 @@
 window._env_ = {
+DEFAULT_TITLE: "Inji Verify",
+DEFAULT_THEME: "purple_theme",
+DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter",
+OVP_QR_HEADER: "INJI_OVP://",
 INTERNET_CONNECTIVITY_CHECK_ENDPOINT: "https://dns.google/",
 INTERNET_CONNECTIVITY_CHECK_TIMEOUT: "10000",
-OVP_QR_HEADER: "INJI_OVP://",
 }
