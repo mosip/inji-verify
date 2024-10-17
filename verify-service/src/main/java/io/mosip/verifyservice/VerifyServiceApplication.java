431ff349a9d4da4e85192e58120128b413162702
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-10-17 09:55:10 +0530

    [INJIVER-586] sample server component (#197)
    * [INJIVER-586] - rename verify UI
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - add example server component
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - add parent pom for verify service
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - ignore build dir
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - update package-lock.json
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - update workflow location
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - update java to 21
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIVER-586] - update version
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

---
+++ verify-service/src/main/java/io/mosip/verifyservice/VerifyServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mosip.verifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerifyServiceApplication.class, args);
+    }
+
+}
