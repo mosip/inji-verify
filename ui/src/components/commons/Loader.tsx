f4d919dba5b801ec737e3963db2d54e114536c66
srikanth716 <srikanthsri7447@gmail.com>
2024-10-24 17:42:19 +0530

    [INJIVER-727] fix loader and color changes
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- ui/src/components/commons/Loader.tsx
+++ ui/src/components/commons/Loader.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-function Loader(props: any) {
+const Loader = (props: { className?: string; innerBg?: string }) => {
   return (
-    <div
-      id="loader"
-      className={`bg-gradient p-[3px] w-[56px] lg:w-[76px] aspect-square rounded-[50%] animate-spin ${
-        props.className ?? ""
-      }`}
-    >
-      <div className={`${props.innerBg??"bg-white"} rounded-full w-full h-full`} />
+    <div id="loader" className="flex justify-center items-center">
+      <div
+        className={`w-16 h-16 rounded-full relative flex items-center justify-center bg-gradient animate-spin ${
+          props.className ?? ""
+        }`}
+      >
+        <div
+          className={`${
+            props.innerBg ?? "bg-white"
+          } w-[85%] h-[85%] rounded-full`}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Loader;
