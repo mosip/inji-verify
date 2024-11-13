955bf19a8bde17d6e534a0f0205bce0c5007f72e
srikanth716 <srikanthsri7447@gmail.com>
2024-11-13 11:12:13 +0530

    [INJIVER-762] implement multi language support and local changes
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- ui/src/utils/i18n.ts
+++ ui/src/utils/i18n.ts
@@ -17,23 +17,27 @@ const resources = { en, ta, kn, hi, fr, ar, pt, es, km };
 export const LanguagesSupported: LanguageObject[] = [
   { label: "Português", value: "pt" },
   { label: "English", value: "en" },
-
+  { label: "தமிழ்", value: "ta" },
+  { label: "ಕನ್ನಡ", value: "kn" },
+  { label: "हिंदी", value: "hi" },
+  { label: "Français", value: "fr" },
+  { label: "عربي", value: "ar" },
+  { label: "español", value: "es" },
+  { label: "ខ្មែរ", value: "km" },
 ];
 
 export const defaultLanguage = window._env_.DEFAULT_LANG;
 
 export const selected_language = storage.getItem(storage.SELECTED_LANGUAGE);
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: selected_language ? selected_language : defaultLanguage,
-    fallbackLng: defaultLanguage,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: selected_language ? selected_language : defaultLanguage,
+  fallbackLng: defaultLanguage,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export const switchLanguage = async (language: string) => {
   storage.setItem(storage.SELECTED_LANGUAGE, language);
