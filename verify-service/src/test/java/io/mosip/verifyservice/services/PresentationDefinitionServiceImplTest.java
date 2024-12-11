26a1563afe9d7066baf5336e78546251b47c4e60
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-12-11 11:44:57 +0530

    [INJIVER-587] -  add unit test
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

---
+++ verify-service/src/test/java/io/mosip/verifyservice/services/PresentationDefinitionServiceImplTest.java
@@ -0,0 +1,51 @@
+package io.mosip.verifyservice.services;
+
+import io.mosip.verifycore.dto.presentation.InputDescriptorDto;
+import io.mosip.verifycore.dto.presentation.PresentationDefinitionDto;
+import io.mosip.verifycore.dto.presentation.SubmissionRequirementDto;
+import io.mosip.verifycore.models.PresentationDefinition;
+import io.mosip.verifyservice.repository.PresentationDefinitionRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class PresentationDefinitionServiceImplTest {
+
+
+    @Test
+    public void shouldReturnValidPresentationDefinitionForGivenId() {
+        PresentationDefinitionRepository mockRepository = mock(PresentationDefinitionRepository.class);
+        List<InputDescriptorDto> mockInputDescriptorDtos = mock();
+        List<SubmissionRequirementDto> mockSubmissionRequirementDtos = mock();
+        PresentationDefinition mockPresentationDefinition = new PresentationDefinition("test_id", mockInputDescriptorDtos, mockSubmissionRequirementDtos);
+        when(mockRepository.findById("test_id")).thenReturn(java.util.Optional.of(mockPresentationDefinition));
+
+        PresentationDefinitionServiceImpl service = new PresentationDefinitionServiceImpl();
+        service.presentationDefinitionRepository = mockRepository;
+
+        PresentationDefinitionDto result = service.getPresentationDefinition("test_id");
+
+        assertNotNull(result);
+        assertEquals("test_id", result.getId());
+        assertEquals(mockInputDescriptorDtos, result.getInputDescriptors());
+        assertEquals(mockSubmissionRequirementDtos, result.getSubmissionRequirements());
+    }
+
+    @Test
+    public void shouldReturnNullIfPresentationDefinitionIsNotFoundForGivenId() {
+        PresentationDefinitionRepository mockRepository = mock(PresentationDefinitionRepository.class);
+        when(mockRepository.findById("non_existent_id")).thenReturn(java.util.Optional.empty());
+
+        PresentationDefinitionServiceImpl service = new PresentationDefinitionServiceImpl();
+        service.presentationDefinitionRepository = mockRepository;
+
+        PresentationDefinitionDto result = service.getPresentationDefinition("non_existent_id");
+
+        assertNull(result);
+    }
+}
