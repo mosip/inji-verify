a362e9fffe36d16e3472dd0ddac3c824da2b6423
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-12-19 16:43:42 +0530

    [INJIVER-846] - add basic logging
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/VPDefinitionServiceImpl.java
+++ verify-service/src/main/java/io/inji/verify/services/VPDefinitionServiceImpl.java
@@ -4,22 +4,24 @@ import io.inji.verify.dto.presentation.VPDefinitionResponseDto;
 import io.inji.verify.models.PresentationDefinition;
 import io.inji.verify.spi.VPDefinitionService;
 import io.inji.verify.repository.PresentationDefinitionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class VPDefinitionServiceImpl implements VPDefinitionService {
 
     @Autowired
     PresentationDefinitionRepository presentationDefinitionRepository;
+
     @Override
     public VPDefinitionResponseDto getPresentationDefinition(String id) {
-        try {
-            PresentationDefinition presentationDefinition = presentationDefinitionRepository.findById(id).orElse(null);
-            assert presentationDefinition != null;
-            return  new VPDefinitionResponseDto(presentationDefinition.getId(),presentationDefinition.getInputDescriptors(),presentationDefinition.getSubmissionRequirements());
-        } catch (AssertionError e) {
-            return null;
+
+        PresentationDefinition presentationDefinition = presentationDefinitionRepository.findById(id).orElse(null);
+        if (presentationDefinition != null) {
+            return new VPDefinitionResponseDto(presentationDefinition.getId(), presentationDefinition.getInputDescriptors(), presentationDefinition.getSubmissionRequirements());
         }
+        return null;
     }
 }
