5695b3568374acc54f60264db4557048c8b1e143
Sreenadh S <32409698+sree96@users.noreply.github.com>
2025-01-01 12:29:36 +0530

    [INJIVER-846] - add tests
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

---
+++ verify-service/src/test/java/io/inji/verify/shared/ConstantsTest.java
@@ -0,0 +1,31 @@
+package io.inji.verify.shared;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConstantsTest {
+
+    @Test
+    public void shouldTestConstants() {
+        // Test integer constants
+        assertEquals(300000, Constants.DEFAULT_EXPIRY);
+
+        // Test String constants
+        assertEquals("/direct-post", Constants.RESPONSE_SUBMISSION_URI);
+        assertEquals("/vp-definition/", Constants.VP_DEFINITION_URI);
+        assertEquals("vp_token", Constants.RESPONSE_TYPE);
+        assertEquals("txn", Constants.TRANSACTION_ID_PREFIX);
+        assertEquals("req", Constants.REQUEST_ID_PREFIX);
+        assertEquals("RsaSignature2018", Constants.RSA_SIGNATURE_2018);
+        assertEquals("Ed25519Signature2018", Constants.ED25519_SIGNATURE_2018);
+        assertEquals("Ed25519Signature2020", Constants.ED25519_SIGNATURE_2020);
+        assertEquals("proof", Constants.KEY_PROOF);
+        assertEquals("type", Constants.KEY_TYPE);
+        assertEquals("jws", Constants.KEY_JWS);
+        assertEquals("verificationMethod", Constants.KEY_VERIFICATION_METHOD);
+        assertEquals("verifiableCredential", Constants.KEY_VERIFIABLE_CREDENTIAL);
+        assertEquals("credential", Constants.KEY_CREDENTIAL);
+        assertEquals("Invalid transaction ID, No requests found for given transaction ID.", Constants.ERR_100);
+        assertEquals("No VP submission found for given transaction ID.", Constants.ERR_101);
+    }
+}
