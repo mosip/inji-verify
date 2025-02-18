d47b2d58598816604eafe7513937f9ccd086b1a6
srikanth716 <srikanthsri7447@gmail.com>
2025-02-18 14:51:19 +0530

    [INJIVER-846] refactor request time out
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-service/src/main/java/io/inji/verify/services/impl/VerifiablePresentationRequestServiceImpl.java
+++ verify-service/src/main/java/io/inji/verify/services/impl/VerifiablePresentationRequestServiceImpl.java
@@ -27,8 +27,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 @Service
 @Slf4j
