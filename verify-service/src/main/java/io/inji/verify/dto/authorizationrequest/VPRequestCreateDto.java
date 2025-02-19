78f676bcfe17ef6b89c189611077ee3121d6d4c3
Srikanth Kola <97477121+srikanth716@users.noreply.github.com>
2025-02-19 17:39:21 +0530

    Merge pull request #327 from srikanth716/release-0.11.x
    [INJIVER-846] refactor client Id validationa

--- verify-service/src/main/java/io/inji/verify/dto/authorizationrequest/VPRequestCreateDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/authorizationrequest/VPRequestCreateDto.java
@@ -1,12 +1,17 @@
 package io.inji.verify.dto.authorizationrequest;
 
 import io.inji.verify.dto.presentation.VPDefinitionResponseDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@NotNull
 public class VPRequestCreateDto {
+    @NotNull(message = "Client Id must not be null")
+    @NotBlank(message = "Client Id must not be empty")
     String clientId;
     String transactionId;
     String presentationDefinitionId;
