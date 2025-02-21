690332d91cdbdce5721a1100982f21cda05d9958
Gk <76690271+gk-XL7@users.noreply.github.com>
2025-02-21 20:18:32 +0530

    [ES-2227] VC card UI changes and minor issue fixes (#346)
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- inji-verify/src/components/Home/VerificationSection/Result/ResultSummary.tsx
+++ inji-verify/src/components/Home/VerificationSection/Result/ResultSummary.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import {ReactComponent as VerificationSuccessMobileIcon} from "../../../../assets/verification-success-icon-mobile.svg";
 import {ReactComponent as VerificationFailedMobileIcon} from "../../../../assets/verification-failed-icon-mobile.svg";
+import { useTranslation } from 'react-i18next';
 
 const backgroundColorMapping: any = {
     EXPIRED: "bg-[#BF7A1C]",
     INVALID: "bg-[#D73E3E]",
-    SUCCESS: "bg-[#4B9D1F]"
-}
-
-const displayMessageMapping = {
-    EXPIRED: "Unfortunately, the given credential is expired!",
-    INVALID: "Unfortunately, the given credential is invalid!",
-    SUCCESS: "Congratulations, the given credential is valid!"
+    SUCCESS: "bg-[#1F9F60]"
 }
 
 const ResultSummary = ({status}: {
     status: "SUCCESS" | "EXPIRED" | "INVALID"
 }) => {
-    const backgroundColor = backgroundColorMapping[status]
+    const backgroundColor = backgroundColorMapping[status];
+    const { t } = useTranslation("vc_result");
     return (
         <div className="grid grid-cols-12 w-full">
             <div className={`col-start-1 col-end-13 h-[170px] lg:h-[186px] w-full ${backgroundColor}`}>
@@ -28,7 +24,7 @@ const ResultSummary = ({status}: {
                     <div className="col-start-1 col-end-13">
                         <p id="vc-result-display-message" className="font-normal text-[16px] text-center">
                             {
-                                displayMessageMapping[status]
+                                status === "SUCCESS" ? t("success") : status === "INVALID" ? t("invalid") : t("expired")
                             }
                         </p>
                     </div>
