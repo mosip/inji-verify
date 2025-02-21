6dfeb62d945e212e9124eeb8aaa5e397e38d2768
GurukiranP <talk2gurukiran@gmail.com>
2025-02-21 17:09:50 +0530

    [ES-2227] UI changes and minor fixes
    Signed-off-by: GurukiranP <talk2gurukiran@gmail.com>

--- inji-verify/src/components/Home/VerificationSection/Result/VcDisplayCard.tsx
+++ inji-verify/src/components/Home/VerificationSection/Result/VcDisplayCard.tsx
@@ -1,44 +1,70 @@
-import React from 'react';
-import {convertToId, convertToTitleCase, getDisplayValue} from "../../../../utils/misc";
+import React from "react";
+import {
+  convertToId,
+  convertToTitleCase,
+  getDisplayValue,
+} from "../../../../utils/misc";
 import StyledButton from "../commons/StyledButton";
-import {ReactComponent as DocumentIcon} from '../../../../assets/document.svg';
-import {useAppDispatch} from "../../../../redux/hooks";
-import {goHomeScreen} from "../../../../redux/features/verification/verification.slice";
+import { ReactComponent as DocumentIcon } from "../../../../assets/document.svg";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-function VcDisplayCard({vc}: {vc: any}) {
-    const dispatch = useAppDispatch();
-    return (
-        <div>
-            <div className={`grid w-[340px] m-auto bg-white rounded-[12px] py-[5px] px-[15px] shadow-lg`}>
-                {
-                    vc ? Object.keys(vc.credentialSubject)
-                        .filter(key => key?.toLowerCase() !== "id" && key?.toLowerCase() !== "type")
-                        .map((key, index) => (
-                            <div className={`py-2.5 px-1 xs:col-end-13 ${(index % 2 === 0) ? "lg:col-start-1 lg:col-end-6" : "lg:col-start-8 lg:col-end-13"}`} key={key}>
-                                <p id={convertToId(key)} className="font-normal text-[11px] break-all">
-                                    {convertToTitleCase(key)}
-                                </p>
-                                <p id={`${convertToId(key)}-value`} className="font-bold text-[12px] break-all">
-                                    {getDisplayValue(vc.credentialSubject[key])}
-                                </p>
-                            </div>
-                        ))
-                        : (
-                            <div className="grid content-center justify-center w-[100%] h-[320px] text-[#000000] opacity-10">
-                                <DocumentIcon/>
-                            </div>
-                        )
-                }
-            </div>
-            <div className="grid content-center justify-center">
-                <StyledButton id="verify-another-qr-code-button" className="mx-auto mt-6 mb-20 lg:mb-6" onClick={() => {
-                    dispatch(goHomeScreen({}))
-                }}>
-                    Verify Another QR code
-                </StyledButton>
-            </div>
-        </div>
-    );
+function VcDisplayCard({ vc }: { vc: any }) {
+  const navigate = useNavigate();
+  const { t } = useTranslation("vc_card");
+  return (
+    <div>
+      <div
+        className={`grid w-[340px] m-auto bg-white rounded-[12px] py-[5px] px-[15px] shadow-lg`}
+      >
+        {vc ? (
+          Object.keys(vc.credentialSubject)
+            .filter(
+              (key) =>
+                key?.toLowerCase() !== "id" && key?.toLowerCase() !== "type"
+            )
+            .map((key, index) => (
+              <div
+                className={`py-2.5 px-1 xs:col-end-13 ${
+                  index % 2 === 0
+                    ? "lg:col-start-1 lg:col-end-6"
+                    : "lg:col-start-8 lg:col-end-13"
+                }`}
+                key={key}
+              >
+                <p
+                  id={convertToId(key)}
+                  className="font-normal text-[11px] break-all"
+                >
+                  {convertToTitleCase(key)}
+                </p>
+                <p
+                  id={`${convertToId(key)}-value`}
+                  className="font-bold text-[12px] break-all"
+                >
+                  {getDisplayValue(vc.credentialSubject[key])}
+                </p>
+              </div>
+            ))
+        ) : (
+          <div className="grid content-center justify-center w-[100%] h-[320px] text-[#000000] opacity-10">
+            <DocumentIcon />
+          </div>
+        )}
+      </div>
+      <div className="grid content-center justify-center">
+        <StyledButton
+          id="verify-another-qr-code-button"
+          className="mx-auto mt-6 mb-20 lg:mb-6 !rounded-xl"
+          onClick={() => {
+            navigate("/application");
+          }}
+        >
+          {t("proceed")}
+        </StyledButton>
+      </div>
+    </div>
+  );
 }
 
 export default VcDisplayCard;
--- inji-verify/src/portals/banking/pages/Loan.js
+++ inji-verify/src/portals/banking/pages/Loan.js
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import relyingPartyService from "../services/relyingPartyService";
 import clientDetails from "../constants/clientDetails";
+import Loader from "../../../components/commons/Loader";
 
 const Loan = () => {
   const { t } = useTranslation("loan_page");
@@ -64,52 +65,69 @@ const Loan = () => {
     }
   };
 
-  return (
-    userInfo && (
-      <div>
-        {successBanner && (
-          <div className="bg-[#57A04B] py-2 flex justify-between">
-            <span className="text-white font-light xl:pl-[10rem] pl-4">
-              {t("success_message")}
-            </span>
-            <img
-              src="assets/images/close_icon.svg"
-              alt="close"
-              className="xl:mr-[5rem] hover:cursor-pointer mr-4"
-              onClick={handleCross}
-            />
+  const steps = [
+    t("apply_loan"),
+    t("verify_tan"),
+    t("fill_details"),
+    t("avail_loan"),
+  ];
+
+  return userInfo ? (
+    <div>
+      {successBanner && (
+        <div className="bg-[#57A04B] py-2 flex justify-between">
+          <span className="text-white font-light xl:pl-[10rem] pl-4">
+            {t("success_message")}
+          </span>
+          <img
+            src="assets/images/close_icon.svg"
+            alt="close"
+            className="xl:mr-[5rem] hover:cursor-pointer mr-4"
+            onClick={handleCross}
+          />
+        </div>
+      )}
+      <div className="pt-2 flex">
+        <div className="relative pr-[6rem] hidden lg:block">
+          <img src="assets/images/mask.svg" className="relative" alt="mask" />
+          <img
+            src="assets/images/bank.svg"
+            className="absolute top-[13%] left-[3%] xl:w-auto w-[550px]"
+            alt="bank"
+          />
+        </div>
+        <div className="lg:px-[4rem] m-auto lg:py-0 py-6 px-4">
+          <div className="text-[2.25rem] xl:w-[100%] m-auto xl:m-0 font-bold flex">
+            {t("get_started")}
+            <p className="mx-4">{userInfo.name}</p>
           </div>
-        )}
-        <div className="pt-2 flex">
-          <div className="relative pr-[4rem] hidden lg:block">
-            <img src="assets/images/mask.svg" className="relative" alt="mask" />
-            <img
-              src="assets/images/bank.svg"
-              className="absolute top-[13%] left-[3%] xl:w-auto w-[550px]"
-              alt="bank"
-            />
+          <p className="my-3 font-semibold text-[1.75rem]">{t("subtext")}</p>
+          <div className="my-4">
+            {steps.map((item, index) => {
+              return (
+                <div className="flex my-2 text-sm font-semibold">
+                  <span className="ml-0 mr-1 border-2 border-[#7F56D9] rounded-[25px] w-5 text-center text-[#7F56D9] text-xs font-bold">{index + 1}</span>
+                  <span className="ml-1 mr-3 items-center flex text-[#514A68]">{item}</span>
+                </div>
+              );
+            })}
           </div>
-          <div className="lg:px-[4rem] m-auto lg:py-0 py-6 px-4">
-            <div className="text-[2rem] xl:w-[100%] m-auto xl:m-0 font-bold flex">
-              {t("get_started")}
-              <p className="mx-4">{userInfo.name}</p>
-            </div>
-            <p className="my-3 font-semibold text-[1.25rem]">{t("subtext")}</p>
-            <div className="mt-4 mb-[1rem] text-[#5A7184] font-semibold">
-              {t("loan_upto")}
-              <span className="text-[#6006A8]">{t("amount")}</span>
-            </div>
-            <button
-              type="button"
-              className="bg-[#6006A8] py-3 sm:px-[8rem] px-[5rem] rounded-md text-white"
-              onClick={handleLoan}
-            >
-              {t("apply_loan")}
-            </button>
+          <div className="mt-4 mb-[1rem] text-[#5A7184] font-semibold">
+            {t("loan_upto")}
+            <span className="text-[#6006A8]">{t("amount")}</span>
           </div>
+          <button
+            type="button"
+            className="bg-[#6006A8] py-3 sm:px-[8rem] px-[5rem] rounded-md text-white"
+            onClick={handleLoan}
+          >
+            {t("apply_for_loan")}
+          </button>
         </div>
       </div>
-    )
+    </div>
+  ) : (
+    <Loader className="align-loading-center" />
   );
 };
 
