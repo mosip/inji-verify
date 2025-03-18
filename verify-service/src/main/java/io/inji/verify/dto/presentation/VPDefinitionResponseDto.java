e07ba8878a095733b299ce75be0493b9c86e63ef
Sreenadh S <32409698+sree96@users.noreply.github.com>
2025-03-18 11:48:21 +0530

    [INJIVER-1068] - update presentaton definition dto field validation as per spec
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/dto/presentation/VPDefinitionResponseDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/presentation/VPDefinitionResponseDto.java
@@ -3,8 +3,12 @@ package io.inji.verify.dto.presentation;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nimbusds.jose.shaded.gson.annotations.SerializedName;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 
 import java.util.List;
 
@@ -12,7 +16,11 @@ import java.util.List;
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class VPDefinitionResponseDto {
+    @NotNull
+    @NotBlank
+    @NotEmpty
     String id;
+    @NonNull
     @JsonProperty("input_descriptors")
     @SerializedName("input_descriptors")
     List<InputDescriptorDto> inputDescriptors;
