6ba9222b64c0a95ceb67600eb76ef5275584c417
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2025-03-20 10:44:26 +0530

    Resolved merge conflicts
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/injiverify/utils/InjiVerifyConfigManager.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/injiverify/utils/InjiVerifyConfigManager.java
@@ -20,12 +20,7 @@ public class InjiVerifyConfigManager extends ConfigManager {
 			Properties props = getproperties(path);
 			// Convert Properties to Map and add to moduleSpecificPropertiesMap
 			for (String key : props.stringPropertyNames()) {
-<<<<<<< HEAD
 				moduleSpecificPropertiesMap.put(key, props.getProperty(key));
-=======
-				String value = System.getenv(key) == null ? props.getProperty(key) : System.getenv(key);
-				moduleSpecificPropertiesMap.put(key, value);
->>>>>>> 4a608c54adcd76f1cf5269fc3485e827853d2455
 			}
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage());
