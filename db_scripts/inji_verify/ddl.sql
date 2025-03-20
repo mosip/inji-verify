a96bd03b75344c625f5055378c2e007183052814
srikanth716 <srikanthsri7447@gmail.com>
2025-03-20 22:43:06 +0530

    resolve merge conflits
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- db_scripts/inji_verify/ddl.sql
+++ db_scripts/inji_verify/ddl.sql
@@ -1,10 +1,6 @@
 \c inji_verify
 
-<<<<<<< HEAD
 \ir ddl/verify-authorization_request_details.sql
-=======
-\ir ddl/verify-authorization_request_create_response.sql
->>>>>>> 8c930c923156c3f26423d5db16c953289b52962c
 \ir ddl/verify-presentation_definition.sql
 \ir ddl/verify-vp_submission.sql
 
