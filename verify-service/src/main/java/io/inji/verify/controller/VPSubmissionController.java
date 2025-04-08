2184bad29e0164b6a37073a44882108df0a4c601
srikanth716 <srikanthsri7447@gmail.com>
2025-04-08 12:45:39 +0530

    [INJIVER-1065] revert unwanted changes
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-service/src/main/java/io/inji/verify/controller/VPSubmissionController.java
+++ verify-service/src/main/java/io/inji/verify/controller/VPSubmissionController.java
@@ -25,8 +25,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Set;
-
 @RestController
 @RequestMapping(path = Constants.RESPONSE_SUBMISSION_URI_ROOT)
 @Slf4j
