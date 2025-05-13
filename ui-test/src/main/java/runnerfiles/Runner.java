43ad7d64b72bdc09fb7f4525dc3cac669c233ba6
Anup Nehe <anup.nehe@technoforte.co.in>
2025-05-13 16:05:57 +0530

    resolve conflicts
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

--- ui-test/src/main/java/runnerfiles/Runner.java
+++ ui-test/src/main/java/runnerfiles/Runner.java
@@ -182,10 +182,10 @@ public class Runner extends AbstractTestNGCucumberTests{
 
 		String path = null;
 		if (getRunType().equalsIgnoreCase("JAR")) {
-			path = new File(jarUrl).getParentFile().getAbsolutePath() + "/";
+			path = new File(jarUrl).getParentFile().getAbsolutePath() + "/MosipTestResource/MosipTemporaryTestResource";
 		} else if (getRunType().equalsIgnoreCase("IDE")) {
 			path = new File(Runner.class.getClassLoader().getResource("").getPath()).getAbsolutePath()
-					+ "/";
+					+ "/MosipTestResource/MosipTemporaryTestResource";
 			if (path.contains(GlobalConstants.TESTCLASSES))
 				path = path.replace(GlobalConstants.TESTCLASSES, "classes");
 		}
