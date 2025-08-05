fe94e458fa46a4c76fb44e9cb60ecfc0d5ab84ab
srikanth716 <srikanthsri7447@gmail.com>
2025-08-05 15:35:25 +0530

    [INJIVER-1255] refactor and added rollback and update sql
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- db_upgrade_script/inji_verify/sql/0.13.0_to_0.14.0_upgrade.sql
+++ db_upgrade_script/inji_verify/sql/0.13.0_to_0.14.0_upgrade.sql
@@ -3,39 +3,46 @@
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 -- -------------------------------------------------------------------------------------------------
--- Rollback Script: v0.13.0 to v0.12.3
+-- Upgrade Script: v0.12.3 to v0.13.0
 -- Database       : inji_verify
--- Purpose        : Revert schema changes introduced in version 0.13.0
+-- Purpose        : Apply schema changes introduced in version 0.13.0
 -- Author         : 
 -- Date           : 
 -- -------------------------------------------------------------------------------------------------
 
 -- -------------------------------------------------------------------------------------------------
--- SECTION 1: Revert column types back to JSONB
+-- SECTION 1: Alter column types (from JSON/JSONB to TEXT)
 -- -------------------------------------------------------------------------------------------------
 
 ALTER TABLE authorization_request_details 
 ALTER COLUMN authorization_details 
-TYPE JSONB 
-USING authorization_details::JSONB;
+TYPE TEXT 
+USING authorization_details::TEXT;
 
 ALTER TABLE presentation_definition 
 ALTER COLUMN vp_format 
-TYPE JSONB 
-USING vp_format::JSONB;
+TYPE TEXT 
+USING vp_format::TEXT;
 
 ALTER TABLE presentation_definition 
 ALTER COLUMN submission_requirements 
-TYPE JSONB 
-USING submission_requirements::JSONB;
+TYPE TEXT 
+USING submission_requirements::TEXT;
 
 ALTER TABLE vp_submission 
 ALTER COLUMN presentation_submission 
-TYPE JSONB 
-USING presentation_submission::JSONB;
+TYPE TEXT 
+USING presentation_submission::TEXT;
 
 -- -------------------------------------------------------------------------------------------------
--- SECTION 2: Drop vc_submission table
+-- SECTION 2: Create vc_submission table
 -- -------------------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS vc_submission;
+CREATE TABLE IF NOT EXISTS vc_submission (
+    transaction_id character varying(40) NOT NULL,
+    vc TEXT NOT NULL
+);
+
+COMMENT ON TABLE vc_submission IS 'VC Submission table: Store details of all the verifiable credentials submissions';
+COMMENT ON COLUMN vc_submission.transaction_id IS 'Transaction ID: transaction ID verifiable credentials submission';
+COMMENT ON COLUMN vc_submission.vc IS 'VC: base64 encoded VC submission result';
