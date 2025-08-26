2849a67d63d8d00e3dd0f49202e06174b361fe55
srikanth716 <srikanthsri7447@gmail.com>
2025-08-26 20:47:51 +0530

    [INJIVER-1186] fix:duplicate element id's
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-ui/src/pages/Verify.tsx
+++ verify-ui/src/pages/Verify.tsx
@@ -29,7 +29,7 @@ export function Verify() {
 
   const renderRequestCredentialsButton = () => (
     <Button
-      id="request-credentials-button"
+      id="stepper-request-credentials-button"
       title={t("rqstButton")}
       className={`w-[300px] mx-auto lg:ml-[76px] mt-10 hidden lg:block`}
       variant="fill"
