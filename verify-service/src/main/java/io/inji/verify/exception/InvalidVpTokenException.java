b1ac867c47ff9bb8a981712f50bcbd3135f8d50e
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-09-19 16:57:25 +0530

    [INJIVER-1311] - change RuntimeException to Exception
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/exception/InvalidVpTokenException.java
+++ verify-service/src/main/java/io/inji/verify/exception/InvalidVpTokenException.java
@@ -1,6 +1,6 @@
 package io.inji.verify.exception;
 
-public class InvalidVpTokenException extends RuntimeException {
+public class InvalidVpTokenException extends Exception {
     private static final String message = "Invalid VP token";
     public InvalidVpTokenException() {
         super(message);
