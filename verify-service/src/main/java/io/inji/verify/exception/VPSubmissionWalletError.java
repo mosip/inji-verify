2b0b2c440a2664ac647c5597fbbc367ffd583cbf
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-09-19 15:12:47 +0530

    [INJIVER-1311] - throw error for non 200 respone status
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/exception/VPSubmissionWalletError.java
+++ verify-service/src/main/java/io/inji/verify/exception/VPSubmissionWalletError.java
@@ -3,11 +3,11 @@ package io.inji.verify.exception;
 import lombok.Getter;
 
 @Getter
-public class VpSubmissionError extends Exception {
+public class VpSubmissionException extends Exception {
     private final String errorCode;
     private final String errorDescription;
 
-    public VpSubmissionError(String errorCode, String errorDescription) {
+    public VpSubmissionException(String errorCode, String errorDescription) {
         super(errorDescription);
         this.errorCode = errorCode;
         this.errorDescription =  errorDescription;
