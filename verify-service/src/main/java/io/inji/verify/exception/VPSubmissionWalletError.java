73e21de5f92c7c1227e800a5e52b23a87f93bc20
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-09-19 16:58:24 +0530

    [INJIVER-1311] - change name from VpSubmissionError to VPSubmissionWalletError
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/exception/VPSubmissionWalletError.java
+++ verify-service/src/main/java/io/inji/verify/exception/VPSubmissionWalletError.java
@@ -3,11 +3,11 @@ package io.inji.verify.exception;
 import lombok.Getter;
 
 @Getter
-public class VpSubmissionError extends Exception {
+public class VPSubmissionWalletError extends Exception {
     private final String errorCode;
     private final String errorDescription;
 
-    public VpSubmissionError(String errorCode, String errorDescription) {
+    public VPSubmissionWalletError(String errorCode, String errorDescription) {
         super(errorDescription);
         this.errorCode = errorCode;
         this.errorDescription =  errorDescription;
