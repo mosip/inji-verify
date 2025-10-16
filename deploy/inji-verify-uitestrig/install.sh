f689b7cb6c4cb045d4676fcf22bef6c7a3f4ffab
Abhishek S <127825992+abhishek8shankar@users.noreply.github.com>
2025-10-16 22:29:40 +0530

    [MOSIP-43325] Updated required configs for ui-testrigs/develop (#840)
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    * [MOSIP-43325] Updated required configs for ui-testrigs/develop
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>
    
    ---------
    
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- deploy/inji-verify-uitestrig/install.sh
+++ deploy/inji-verify-uitestrig/install.sh
@@ -6,8 +6,8 @@ if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 
-NS=injiverify-ui-testrig
-CHART_VERSION=1.3.4
+NS=uitestrig-injiverify
+CHART_VERSION=0.0.1-develop
 COPY_UTIL=../copy_cm_func.sh
 
 echo Create $NS namespace
@@ -16,19 +16,8 @@ kubectl create ns $NS
 function installing_uitestrig() {
   ENV_NAME=$( kubectl -n default get cm global -o json |jq -r '.data."installation-domain"')
 
-  read -p "Please enter the time(hr) to run the cronjob every day (time: 0-23) : " time
-  if [ -z "$time" ]; then
-     echo "ERROR: Time cannot be empty; EXITING;";
-     exit 1;
-  fi
-  if ! [ $time -eq $time ] 2>/dev/null; then
-     echo "ERROR: Time $time is not a number; EXITING;";
-     exit 1;
-  fi
-  if [ $time -gt 23 ] || [ $time -lt 0 ] ; then
-     echo "ERROR: Time should be in range ( 0-23 ); EXITING;";
-     exit 1;
-  fi
+  read -p "Is values.yaml under inji-verify-uitestrig set correctly as part of pre-requisites? (Y/n) : " yn;
+  if [[ $yn = "Y" ]] || [[ $yn = "y" ]] ; then
 
   echo "Do you have public domain & valid SSL? (Y/n) "
   echo "Y: if you have public domain & valid ssl certificate"
@@ -39,46 +28,11 @@ function installing_uitestrig() {
     echo "'flag' was provided; EXITING;"
     exit 1;
   fi
+
   ENABLE_INSECURE=''
   if [ "$flag" = "n" ]; then
     ENABLE_INSECURE='--set uitestrig.configmaps.uitestrig.ENABLE_INSECURE=true';
   fi
-
-  read -p "Please enter the MOSIP_INJIWEB_GOOGLE_REFRESH_TOKEN : " token
-    if [ -z "$token" ]; then
-       echo "ERROR: Google Refresh Token cannot be empty; EXITING;";
-       exit 1;
-    fi
-
-  read -p "Please enter the MOSIP_INJIWEB_GOOGLE_CLIENT_ID : " client_id
-    if [ -z "$client_id" ]; then
-       echo "ERROR: Google Client ID cannot be empty; EXITING;";
-       exit 1;
-    fi
-
-  read -p "Please enter the MOSIP_INJIWEB_GOOGLE_CLIENT_SECRET : " secret
-    if [ -z "$secret" ]; then
-       echo "ERROR: Google client secret cannot be empty; EXITING;";
-       exit 1;
-    fi
-
-  read -p "Please enter the BROWSERSTACK USERNAME : " User_name
-    if [ -z "$User_name" ]; then
-       echo "ERROR: BROWSERSTACK USERNAME cannot be empty; EXITING;";
-       exit 1;
-    fi
-
-  read -p "Please enter the BROWSERSTACK ACCESS KEY : " Access_key
-    if [ -z "$Access_key" ]; then
-       echo "ERROR: BROWSERSTACK ACCESS KEY cannot be empty; EXITING;";
-       exit 1;
-    fi
-
-    read -p "Please enter the Env user : " Env_user
-      if [ -z "$Env_user" ]; then
-         echo "ERROR: Env user cannot be empty; EXITING;";
-         exit 1;
-      fi
 
   echo Istio label
   kubectl label ns $NS istio-injection=disabled --overwrite
@@ -87,7 +41,6 @@ function installing_uitestrig() {
   echo Copy configmaps
   $COPY_UTIL configmap global default $NS
   $COPY_UTIL configmap keycloak-host keycloak $NS
-  $COPY_UTIL configmap artifactory-share artifactory $NS
   $COPY_UTIL configmap config-server-share config-server $NS
 
   echo Copy secrets
@@ -96,47 +49,19 @@ function installing_uitestrig() {
   $COPY_UTIL secret postgres-postgresql postgres $NS
 
   DB_HOST=$( kubectl -n default get cm global -o json  |jq -r '.data."mosip-api-internal-host"' )
-  PMP_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-pmp-host"')
-  ADMIN_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-admin-host"')
-  RESIDENT_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-resident-host"')
   INJI_VERIFY_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-injiverify-host"')
   INJI_WEB_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-injiweb-host"')
   ESIGNET_HOST=$(kubectl -n default get cm global -o json  |jq -r '.data."mosip-esignet-host"')
   API_INTERNAL_HOST=$( kubectl -n default get cm global -o json  |jq -r '.data."mosip-api-internal-host"' )
 
   echo Installing verify uitestrig
-  helm -n $NS install verify-uitestrig mosip/uitestrig \
-  --set crontime="0 $time * * *" \
+  helm -n $NS install uitestrig-injiverify mosip/uitestrig \
   -f values.yaml  \
   --version $CHART_VERSION \
-  --set uitestrig.configmaps.s3.s3-host='http://minio.minio:9000' \
-  --set uitestrig.configmaps.s3.s3-user-key='admin' \
-  --set uitestrig.configmaps.s3.s3-region='' \
-  --set uitestrig.configmaps.db.db-server="$DB_HOST" \
-  --set uitestrig.configmaps.db.db-su-user="postgres" \
-  --set uitestrig.configmaps.db.db-port="5432" \
-  --set uitestrig.configmaps.uitestrig.apiInternalEndPoint="https://$API_INTERNAL_HOST" \
-  --set uitestrig.configmaps.uitestrig.apiEnvUser="$API_INTERNAL_HOST" \
-  --set uitestrig.configmaps.uitestrig.PmpPortalPath="https://$PMP_HOST" \
-  --set uitestrig.configmaps.uitestrig.adminPortalPath="https://$ADMIN_HOST" \
-  --set uitestrig.configmaps.uitestrig.residentPortalPath="https://$RESIDENT_HOST" \
-  --set uitestrig.configmaps.uitestrig.verifyPortalPath="https://$INJI_VERIFY_HOST/" \
-  --set uitestrig.configmaps.uitestrig.NS="$NS" \
-  --set uitestrig.configmaps.uitestrig.mosip_components_base_urls="auditmanager=$API_INTERNAL_HOST;idrepository=$API_INTERNAL_HOST;partnermanager=$API_INTERNAL_HOST;idauthentication=$API_INTERNAL_HOST;policymanager=$API_INTERNAL_HOST;authmanager=$API_INTERNAL_HOST;resident=$API_INTERNAL_HOST;preregistration=$API_INTERNAL_HOST;masterdata=$API_INTERNAL_HOST;idgenerator=$API_INTERNAL_HOST;" \
-  --set uitestrig.configmaps.uitestrig.mosip_inji_web_url="https://$INJI_WEB_HOST/" \
-  --set uitestrig.configmaps.uitestrig.injiweb="https://$INJI_WEB_HOST/issuers" \
-  --set uitestrig.configmaps.uitestrig.eSignetbaseurl="https://$ESIGNET_HOST" \
-  --set uitestrig.configmaps.uitestrig.injiverify="https://$INJI_VERIFY_HOST/" \
-  --set uitestrig.configmaps.uitestrig.ENV_ENDPOINT="https://$API_INTERNAL_HOST" \
-  --set uitestrig.configmaps.uitestrig.ENV_USER="$Env_user" \
-  --set uitestrig.configmaps.uitestrig.MOSIP_INJIWEB_GOOGLE_REFRESH_TOKEN="$token" \
-  --set uitestrig.configmaps.uitestrig.MOSIP_INJIWEB_GOOGLE_CLIENT_ID="$client_id" \
-  --set uitestrig.configmaps.uitestrig.MOSIP_INJIWEB_GOOGLE_CLIENT_SECRET="$secret" \
-  --set uitestrig.configmaps.uitestrig.BROWSERSTACK_ACCESS_KEY="$Access_key" \
-  --set uitestrig.configmaps.uitestrig.BROWSERSTACK_USERNAME="$User_name" \
   $ENABLE_INSECURE
 
   echo Installed verify uitestrig
+  fi
   return 0
 }
 
