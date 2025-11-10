1a5e8fae70bcfbe80b60c286851fee5ab2d4dddc
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-11-10 08:19:41 +0530

    [INJIVER-1358] - add utils method getVcVerificationStatus
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/utils/Utils.java
+++ verify-service/src/main/java/io/inji/verify/utils/Utils.java
@@ -1,9 +1,16 @@
 package io.inji.verify.utils;
 
+import io.inji.verify.shared.Constants;
+import io.mosip.vercred.vcverifier.data.CredentialStatusResult;
+import io.mosip.vercred.vcverifier.data.CredentialVerificationSummary;
+import io.mosip.vercred.vcverifier.data.VerificationResult;
+import io.mosip.vercred.vcverifier.data.VerificationStatus;
 import io.mosip.vercred.vcverifier.utils.Base64Decoder;
+import io.mosip.vercred.vcverifier.utils.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -32,5 +39,24 @@ public final class Utils {
     private static String decodeBase64Json(String encoded)  {
         byte[] decodedBytes = new Base64Decoder().decodeFromBase64Url(encoded);
         return new String(decodedBytes);
+    }
+
+    public static VerificationStatus getVcVerificationStatus(CredentialVerificationSummary credentialVerificationSummary) {
+        log.info("Credential Verification Summary: {}", credentialVerificationSummary);
+        VerificationResult verificationResult = credentialVerificationSummary.getVerificationResult();
+        VerificationStatus verificationStatus = Util.INSTANCE.getVerificationStatus(verificationResult);
+        log.info("VC Verification Status:: {}", verificationStatus);
+
+        List<CredentialStatusResult> credentialStatusResults = credentialVerificationSummary.getCredentialStatus();
+        log.info("Credential Status: {}", credentialStatusResults);
+
+        if (!credentialStatusResults.isEmpty()) {
+            String purpose = credentialStatusResults.getFirst().getPurpose();
+            int status = credentialStatusResults.getFirst().getStatus();
+            boolean isRevoked = purpose.equalsIgnoreCase(Constants.STATUS_PURPOSE_REVOKED);
+            if (isRevoked && status == 1) return VerificationStatus.REVOKED;
+        }
+
+        return verificationStatus;
     }
 }
