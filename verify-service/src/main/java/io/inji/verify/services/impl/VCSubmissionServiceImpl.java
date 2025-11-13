7ae89e86cff5ce09a845c9802e1ee5e43c314516
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-11-13 17:44:15 +0530

    [INJIVER-1308] - handle sdjwt vc verification
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/impl/VCSubmissionServiceImpl.java
+++ verify-service/src/main/java/io/inji/verify/services/impl/VCSubmissionServiceImpl.java
@@ -1,9 +1,11 @@
 package io.inji.verify.services.impl;
 
 import io.mosip.vercred.vcverifier.data.CredentialVerificationSummary;
+import io.mosip.vercred.vcverifier.data.VerificationResult;
 import io.mosip.vercred.vcverifier.data.VerificationStatus;
+import io.mosip.vercred.vcverifier.utils.Util;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import io.inji.verify.dto.submission.VCSubmissionDto;
 import io.inji.verify.dto.submission.VCSubmissionResponseDto;
 import io.inji.verify.dto.submission.VCSubmissionVerificationStatusDto;
@@ -16,7 +18,9 @@ import io.mosip.vercred.vcverifier.CredentialsVerifier;
 import io.mosip.vercred.vcverifier.constants.CredentialFormat;
 import java.util.ArrayList;
 import java.util.List;
+import static io.inji.verify.utils.Utils.isSdJwt;
 
+@Slf4j
 @Service
 public class VCSubmissionServiceImpl implements VCSubmissionService {
 
@@ -40,13 +44,23 @@ public class VCSubmissionServiceImpl implements VCSubmissionService {
     @Override
     public VCSubmissionVerificationStatusDto getVcWithVerification(String transactionId) {
         return vcSubmissionRepository.findById(transactionId).map(vcSubmission -> {
-            String vcJSON = vcSubmission.getVc();
+            String vc = vcSubmission.getVc();
+            boolean isSdJwtVc = isSdJwt(vc);
+            if (isSdJwtVc) {
+                VerificationResult verificationResult = credentialsVerifier.verify(vc, CredentialFormat.VC_SD_JWT);
+                if (!verificationResult.getVerificationStatus()) {
+                    log.error("SD-JWT VC verification result errors : {} {}", verificationResult.getVerificationErrorCode(), verificationResult.getVerificationMessage());
+                }
+                VerificationStatus status = Util.INSTANCE.getVerificationStatus(verificationResult);
+                return new VCSubmissionVerificationStatusDto(vc, status);
+            }
+
             List<String> statusPurposeList = new ArrayList<>();
             statusPurposeList.add(Constants.STATUS_PURPOSE_REVOKED);
-            CredentialVerificationSummary credentialVerificationSummary = credentialsVerifier.verifyAndGetCredentialStatus(vcJSON, CredentialFormat.LDP_VC, statusPurposeList);
+            CredentialVerificationSummary credentialVerificationSummary = credentialsVerifier.verifyAndGetCredentialStatus(vc, CredentialFormat.LDP_VC, statusPurposeList);
             VerificationStatus vcVerificationStatus = Utils.getVcVerificationStatus(credentialVerificationSummary);
 
-            return new VCSubmissionVerificationStatusDto(vcJSON, vcVerificationStatus);
+            return new VCSubmissionVerificationStatusDto(vc, vcVerificationStatus);
         }).orElse(null);
     }
 }
