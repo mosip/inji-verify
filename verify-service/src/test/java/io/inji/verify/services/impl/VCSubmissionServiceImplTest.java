f4cd4b1bde634dd038fdb842853db7542897aa8b
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-11-14 10:47:19 +0530

    [INJIVER-1308] - add sdjwtVC test
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/test/java/io/inji/verify/services/impl/VCSubmissionServiceImplTest.java
+++ verify-service/src/test/java/io/inji/verify/services/impl/VCSubmissionServiceImplTest.java
@@ -8,7 +8,6 @@ import io.inji.verify.shared.Constants;
 import io.inji.verify.utils.Utils;
 import io.mosip.vercred.vcverifier.CredentialsVerifier;
 import io.mosip.vercred.vcverifier.constants.CredentialFormat;
-import io.mosip.vercred.vcverifier.constants.CredentialValidatorConstants;
 import io.mosip.vercred.vcverifier.data.CredentialVerificationSummary;
 import io.mosip.vercred.vcverifier.data.VerificationResult;
 import io.mosip.vercred.vcverifier.data.VerificationStatus;
@@ -18,14 +17,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
 import io.inji.verify.dto.submission.VCSubmissionDto;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,6 +35,7 @@ public class VCSubmissionServiceImplTest {
     private VCSubmissionServiceImpl vcSubmissionService;
 
     private final String TEST_VC_STRING = "{\"vc\": \"someVerifiableCredential\"}";
+    private final String TEST_SDJWT_VC_STRING = "{\"vc\":\"eyJhbGciOiJSUzI1NiIsInR5cCI6In\"}";
     private final String TEST_TRANSACTION_ID = "txn-12345";
 
     @Test
@@ -61,6 +56,28 @@ public class VCSubmissionServiceImplTest {
             verify(vcSubmissionRepository, times(1)).save(argThat(vc ->
                     vc.getTransactionId().equals(TEST_TRANSACTION_ID) &&
                             vc.getVc().equals(TEST_VC_STRING)
+            ));
+        }
+    }
+
+    @Test
+    void submitSDJWTVC_shouldSaveVCAndReturnResponseDto() {
+        try (MockedStatic<Utils> mockedUtils = mockStatic(Utils.class)) {
+            mockedUtils.when(() -> Utils.generateID(Constants.TRANSACTION_ID_PREFIX))
+                    .thenReturn(TEST_TRANSACTION_ID);
+
+            VCSubmissionDto submissionDto = new VCSubmissionDto(TEST_SDJWT_VC_STRING, null);
+            VCSubmission expectedVCSubmission = new VCSubmission(TEST_TRANSACTION_ID, TEST_VC_STRING);
+            when(vcSubmissionRepository.save(any(VCSubmission.class))).thenReturn(expectedVCSubmission);
+
+            VCSubmissionResponseDto response = vcSubmissionService.submitVC(submissionDto);
+
+            assertNotNull(response);
+            assertEquals(TEST_TRANSACTION_ID, response.getTransactionId());
+
+            verify(vcSubmissionRepository, times(1)).save(argThat(vc ->
+                    vc.getTransactionId().equals(TEST_TRANSACTION_ID) &&
+                            vc.getVc().equals(TEST_SDJWT_VC_STRING)
             ));
         }
     }
@@ -108,6 +125,32 @@ public class VCSubmissionServiceImplTest {
                     argThat(list -> list.contains(Constants.STATUS_PURPOSE_REVOKED))
             );
             utilsMock.verify(() -> Utils.getVcVerificationStatus(credentialVerificationSummary), times(1));
+        }
+    }
+
+    @Test
+    void getVcWithVerification_shouldReturnSuccessStatus_whenSDJWTVerificationPasses() {
+        try (MockedStatic<Utils> utilsMock = mockStatic(Utils.class)) {
+            VCSubmission foundVCSubmission = new VCSubmission(TEST_TRANSACTION_ID, TEST_SDJWT_VC_STRING);
+            when(vcSubmissionRepository.findById(TEST_TRANSACTION_ID)).thenReturn(Optional.of(foundVCSubmission));
+
+            VerificationResult successResult = mock(VerificationResult.class);
+            when(successResult.getVerificationStatus()).thenReturn(true);
+            when(successResult.getVerificationErrorCode()).thenReturn("");
+
+            when(credentialsVerifier.verify(TEST_SDJWT_VC_STRING, CredentialFormat.VC_SD_JWT))
+                    .thenReturn(successResult);
+
+            utilsMock.when(() -> Utils.isSdJwt(TEST_SDJWT_VC_STRING)).thenReturn(true);
+
+            VCSubmissionVerificationStatusDto resultDto = vcSubmissionService.getVcWithVerification(TEST_TRANSACTION_ID);
+
+            assertNotNull(resultDto);
+            assertEquals(TEST_SDJWT_VC_STRING, resultDto.getVc());
+            assertEquals(VerificationStatus.SUCCESS, resultDto.getVerificationStatus());
+
+            verify(vcSubmissionRepository, times(1)).findById(TEST_TRANSACTION_ID);
+            verify(credentialsVerifier, times(1)).verify(TEST_SDJWT_VC_STRING, CredentialFormat.VC_SD_JWT);
         }
     }
 
