a30b65eb13419c74f50bdaafd72c51a4fdebaea4
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-11-17 17:56:41 +0530

    [INJIVER-1387] - update db-upgrade-script
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

---
+++ db_upgrade_script/inji_verify/sql/0.15.1_to_0.16.0_upgrade.sql
@@ -0,0 +1,9 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+-- -------------------------------------------------------------------------------------------------
+-- Upgrade Script : v0.15.1 to v0.16.0
+-- Database       : inji_verify
+-- Purpose        : Apply schema changes introduced in version 0.16.0
+-- -------------------------------------------------------------------------------------------------
