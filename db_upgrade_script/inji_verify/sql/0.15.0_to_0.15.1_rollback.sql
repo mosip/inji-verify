619618c85d50ee1b66367cea06299ffa519faee2
mayuradesh <mayura.deshmukh@gmail.com>
2025-11-17 17:33:30 +0530

    added files for db upgrade scripts
    Signed-off-by: mayuradesh <mayura.deshmukh@gmail.com>

---
+++ db_upgrade_script/inji_verify/sql/0.15.0_to_0.15.1_rollback.sql
@@ -0,0 +1,9 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+-- -------------------------------------------------------------------------------------------------
+-- Rollback Script: v0.15.0 to v0.15.1
+-- Database       : inji_verify
+-- Purpose        : Revert schema changes introduced in version 0.15.1
+-- -------------------------------------------------------------------------------------------------
