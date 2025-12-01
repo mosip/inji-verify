9d73ce04919e3e8428e74d4c6552541219635988
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-01 13:42:54 +0530

    INJIVER 1418 fixed LSH comments 0.16.0 in develop branch (#974)
    * [INJIVER-1418] - add null chks for data.error add chk for AppError
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    * [INJIVER-1418] - remove type QrData
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    * [INJIVER-1418] - import QrData from OVPSchemeQrData & rename us to userAgent
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    * [INJIVER-1418] - bump up sdk version
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- inji-verify-sdk/src/components/qrcode-verification/QRCodeVerification.types.ts
+++ inji-verify-sdk/src/components/qrcode-verification/QRCodeVerification.types.ts
@@ -109,20 +109,3 @@ export interface vcSubmissionBody {
   vc: any;
   transactionId?: string;
 }
-
-export interface QrData {
-  transactionId: string;
-  requestId: string;
-  authorizationDetails?: {
-    responseType: string;
-    responseMode: string;
-    clientId: string;
-    presentationDefinition: Record<string, unknown>;
-    presentationDefinitionUri?: string;
-    responseUri: string;
-    nonce: string;
-    iat: number;
-  };
-  expiresAt: number;
-  requestUri?: string;
-}
