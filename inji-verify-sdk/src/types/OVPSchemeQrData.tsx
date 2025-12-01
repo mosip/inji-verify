124e7455be3b80c50c0a91df9d7b78378df93150
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-01 13:51:13 +0530

    [INJIVER-1418] - add OVPSchemeQrData.ts
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

---
+++ inji-verify-sdk/src/types/OVPSchemeQrData.tsx
@@ -0,0 +1,16 @@
+export interface QrData {
+  transactionId: string;
+  requestId: string;
+  authorizationDetails?: {
+    responseType: string;
+    responseMode: string;
+    clientId: string;
+    presentationDefinition: Record<string, unknown>;
+    presentationDefinitionUri?: string;
+    responseUri: string;
+    nonce: string;
+    iat: number;
+  };
+  expiresAt: number;
+  requestUri?: string;
+}
