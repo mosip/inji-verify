85db99e8f647e559e836399e2d7db7382afa5a6a
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-03 17:24:01 +0530

    [INJIVER-1456] - catch CredentialStatusCheckException
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
+++ verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
@@ -1,23 +1,24 @@
 package io.inji.verify.controller;
 
 import java.util.Optional;
-
 import io.inji.verify.dto.core.ErrorDto;
 import io.inji.verify.dto.submission.VPTokenResultDto;
 import io.inji.verify.enums.ErrorCode;
+import io.inji.verify.exception.CredentialStatusCheckException;
 import io.inji.verify.exception.VPSubmissionNotFoundException;
 import io.inji.verify.exception.VPSubmissionWalletError;
 import io.inji.verify.services.VCSubmissionService;
 import io.inji.verify.services.VerifiablePresentationRequestService;
 import io.inji.verify.services.VerifiablePresentationSubmissionService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import static io.inji.verify.utils.Utils.getResponseEntityForCredentialStatusException;
 
 @RestController
 @Slf4j
@@ -34,7 +35,7 @@ public class VPResultController {
     }
 
     @GetMapping(path = "/vp-result/{transactionId}")
-    public ResponseEntity<Object> getVPResult(@PathVariable String transactionId) {
+    public ResponseEntity<Object> getVPResult(@PathVariable String transactionId, HttpServletRequest request) {
         List<String> requestIds = verifiablePresentationRequestService.getLatestRequestIdFor(transactionId);
 
         if (!requestIds.isEmpty()) {
@@ -48,6 +49,10 @@ public class VPResultController {
             } catch (VPSubmissionWalletError e) {
                 log.error("Received wallet error for transactionId: {} - {} - {}", e.getErrorCode(), e.getErrorDescription(), transactionId);
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto(e.getErrorCode(), e.getErrorDescription()));
+            } catch (CredentialStatusCheckException ex) {
+                log.error("Received Credential Status Check error for " +
+                        "transactionId: {} - {} - {}: ", ex.getErrorCode(), ex.getErrorDescription(), transactionId);
+                return getResponseEntityForCredentialStatusException(ex, request);
             }
         } else {
             return Optional.ofNullable(vcSubmissionService.getVcWithVerification(transactionId))
