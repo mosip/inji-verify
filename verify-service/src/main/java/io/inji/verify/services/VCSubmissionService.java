74ecdec4c079998c2052939dca9146003f8e28c0
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-03 18:05:34 +0530

    [INJIVER-1456] - handle CredentialStatusCheckException
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/VCSubmissionService.java
+++ verify-service/src/main/java/io/inji/verify/services/VCSubmissionService.java
@@ -3,9 +3,10 @@ package io.inji.verify.services;
 import io.inji.verify.dto.submission.VCSubmissionDto;
 import io.inji.verify.dto.submission.VCSubmissionResponseDto;
 import io.inji.verify.dto.submission.VCSubmissionVerificationStatusDto;
+import io.inji.verify.exception.CredentialStatusCheckException;
 
 public interface VCSubmissionService {
     VCSubmissionResponseDto submitVC(VCSubmissionDto vcSubmissionDto);
 
-    VCSubmissionVerificationStatusDto getVcWithVerification(String transactionId);
+    VCSubmissionVerificationStatusDto getVcWithVerification(String transactionId) throws CredentialStatusCheckException;
 }
